2f5568a35dae09057931237e68fd895afda00d2c
Mickaël Canévet <mickael.canevet@gmail.com>
2020-09-18 16:06:00 +0200

    We don't use LE

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,6 @@
 #!/bin/sh -xe
 
-CURL_CA_BUNDLE=/tmp/cert.pem
-
-# Trust Fake LE certificate
-cp /etc/ssl/cert.pem $CURL_CA_BUNDLE
-curl https://letsencrypt.org/certs/fakeleintermediatex1.pem >> $CURL_CA_BUNDLE
-
 BACKDOOR_IP=$(getent hosts argocd.apps."$CLUSTER_NAME"."$BASE_DOMAIN"|cut -f1 -d' ')
 
-curl -I "https://argocd.apps.$CLUSTER_NAME.$BASE_DOMAIN"
-curl -I --resolve "argocd.apps.$BASE_DOMAIN:443:$BACKDOOR_IP" "https://argocd.apps.$BASE_DOMAIN"
+curl -k -I "https://argocd.apps.$CLUSTER_NAME.$BASE_DOMAIN"
+curl -k -I --resolve "argocd.apps.$BASE_DOMAIN:443:$BACKDOOR_IP" "https://argocd.apps.$BASE_DOMAIN"
