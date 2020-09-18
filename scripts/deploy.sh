af5dbe8a67afd0fe78e137559af34c52686ced97
Mickaël Canévet <mickael.canevet@gmail.com>
2020-09-18 03:51:49 +0200

    Fix argocd namespace creation

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 # Install ArgoCD if not present
 if test $(kubectl -n argocd get pods --selector "app.kubernetes.io/name=argocd-server" --output=name|wc -l) -eq 0; then
   helm dependency update argocd/argocd
-  kubectl get ns argocd 2> /dev/null || kubectl create namespace argocd
+  kubectl create namespace argocd || true
   helm template --include-crds argocd argocd/argocd \
     --set bootstrap=true \
     --namespace argocd | kubectl $KUBECTL_COMMAND -n argocd -f -
