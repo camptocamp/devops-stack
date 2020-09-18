4125148b7c28a8fe349d89113c57ed112664631d
Mickaël Canévet <mickael.canevet@gmail.com>
2020-09-18 06:12:25 +0200

    Add missing closing double quote

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,5 +8,5 @@ curl https://letsencrypt.org/certs/fakeleintermediatex1.pem >> $CURL_CA_BUNDLE
 
 BACKDOOR_IP=$(getent hosts argocd.apps."$CLUSTER_NAME"."$BASE_DOMAIN"|cut -f1 -d' ')
 
-curl -I "https://argocd.apps.$CLUSTER_NAME.$BASE_DOMAIN
+curl -I "https://argocd.apps.$CLUSTER_NAME.$BASE_DOMAIN"
 curl -I --resolve "argocd.apps.$BASE_DOMAIN:443:$BACKDOOR_IP" "https://argocd.apps.$BASE_DOMAIN"
