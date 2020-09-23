b0af71739e7e26c8749d6d33f39fb1a529c17a64
Mickaël Canévet <mickael.canevet@gmail.com>
2020-09-23 13:29:14 +0200

    Support deploying multiple cluster in parallel (fixes #58)

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -xe
 
+
+wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -O ~/jq
+chmod +x ~/jq
 cd terraform || exit
 terraform init -upgrade
 terraform workspace select "$CLUSTER_NAME" || terraform workspace new "$CLUSTER_NAME"
@@ -15,4 +18,6 @@ spec:
   source:
     repoURL: $REPO_URL
     targetRevision: $CLUSTER_NAME
+
+baseDomain: $(~/jq -r '.values.root_module.resources[]|select(.type=="docker_container" and .name=="k3s_server").values.ip_address|gsub("\\.";"-") + ".nip.io"' "../$ARTIFACTS_DIR/terraform.tfstate.json")
 EOF
