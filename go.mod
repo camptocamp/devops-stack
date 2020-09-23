0e18cdd1e06b1b8781b5c68339acee74f19509d6
Raphaël Pinson <raphael.pinson@camptocamp.com>
2020-09-23 17:30:11 +0200

    Start converting Makefile to mage

---
+++ go.mod
@@ -0,0 +1,9 @@
+module k8s-demo/m
+
+go 1.14
+
+require (
+	github.com/Netflix/go-env v0.0.0-20200908232752-3e802f601e28
+	github.com/magefile/mage v1.10.0
+	gopkg.in/src-d/go-git.v4 v4.13.1
+)
---
+++ make.go
@@ -0,0 +1,102 @@
+//+build mage
+
+// Mostly this is used for building the website and some dev tasks.
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"strings"
+
+	env "github.com/Netflix/go-env"
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+	"gopkg.in/src-d/go-git.v4"
+	gitconfig "gopkg.in/src-d/go-git.v4/config"
+)
+
+type Environment struct {
+	BaseDomain string `env:"BASE_DOMAIN,default=127-0-0-1.nip.io"`
+	DockerHost string `env:"DOCKER_HOST,default=tcp://127.0.0.1:2376/"`
+
+	RepoUrl      string
+	Remote       string
+	RemoteBranch string
+}
+
+var environment Environment
+
+// Builds the website.  If needed, it will compact the js as well.
+func Build() error {
+	mg.Deps(Env)
+	//fmt.Printf(environment.BaseDomain)
+
+	out, err := sh.Output("echo", "truc")
+	if err != nil {
+		return err
+	}
+	fmt.Printf(out)
+	return nil
+}
+
+func Debug() error {
+	mg.Deps(Env)
+
+	fmt.Println("BASE_DOMAIN =", environment.BaseDomain)
+	fmt.Println("DOCKER_HOST =", environment.DockerHost)
+
+	usr, _ := user.Current()
+	fmt.Println("UID =", usr.Uid)
+	fmt.Println("GID =", usr.Gid)
+
+	fmt.Println("REPO_URL =", environment.RepoUrl)
+	fmt.Println("REMOTE =", environment.Remote)
+	fmt.Println("REMOTE_BRANCH =", environment.RemoteBranch)
+
+	return nil
+}
+
+func Env() error {
+	es, err := env.UnmarshalFromEnviron(&environment)
+	if err != nil {
+		return err
+	}
+
+	if ciProjectUrl, ok := es["CI_PROJECT_URL"]; ok {
+		environment.RepoUrl = ciProjectUrl
+		environment.RemoteBranch = es["CI_COMMIT_REF_NAME"]
+	} else if githubServerUrl, ok := es["GITHUB_SERVER_URL"]; ok {
+		environment.RepoUrl = fmt.Sprintf("%s/%s.git", githubServerUrl, es["GITHUB_REPOSITORY"])
+		//environment.RemoteBranch = es["CI_COMMIT_REF_NAME"]
+	} else {
+		br, err := getCurrentBranch()
+		if err != nil {
+			return err
+		}
+		environment.Remote = br.Remote
+		environment.RemoteBranch = br.Name
+	}
+
+	return err
+}
+
+func getCurrentBranch() (*gitconfig.Branch, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+	repo, err := git.PlainOpen(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	h, err := repo.Head()
+	if err != nil {
+		return nil, err
+	}
+
+	b := strings.TrimPrefix(h.Name().String(), "refs/heads/")
+	fmt.Println("looking for branch", b)
+	return repo.Branch(b)
+}
