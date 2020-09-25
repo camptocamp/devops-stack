a8f3793e17e742f09d576f700ee4d1968a035d31
Mickaël Canévet <mickael.canevet@gmail.com>
2020-09-25 07:58:12 +0200

    Don't create 2 vhost per service

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,19 +1,13 @@
 #!/bin/sh -xe
 
-BACKDOOR_IP=$(getent hosts argocd.apps."$CLUSTER_NAME"."$BASE_DOMAIN"|cut -f1 -d' ')
-
 # ArgoCD
-curl -k -I "https://argocd.apps.$CLUSTER_NAME.$BASE_DOMAIN"
-curl -k -I --resolve "argocd.apps.$BASE_DOMAIN:443:$BACKDOOR_IP" "https://argocd.apps.$BASE_DOMAIN"
+curl -k -I "https://argocd.apps.$BASE_DOMAIN"
 
 # Prometheus
-curl -k -I "https://prometheus.apps.$CLUSTER_NAME.$BASE_DOMAIN"
-curl -k -I --resolve "prometheus.apps.$BASE_DOMAIN:443:$BACKDOOR_IP" "https://prometheus.apps.$BASE_DOMAIN"
+curl -k -I "https://prometheus.apps.$BASE_DOMAIN"
 
 # Alertmanager
-curl -k -I "https://alertmanager.apps.$CLUSTER_NAME.$BASE_DOMAIN"
-curl -k -I --resolve "alertmanager.apps.$BASE_DOMAIN:443:$BACKDOOR_IP" "https://alertmanager.apps.$BASE_DOMAIN"
+curl -k -I "https://alertmanager.apps.$BASE_DOMAIN"
 
 # Grafana
-curl -k -I "https://grafana.apps.$CLUSTER_NAME.$BASE_DOMAIN"
-curl -k -I --resolve "grafana.apps.$BASE_DOMAIN:443:$BACKDOOR_IP" "https://grafana.apps.$BASE_DOMAIN"
+curl -k -I "https://grafana.apps.$BASE_DOMAIN"
