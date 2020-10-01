25e9f3f5c995898fecd87d7233d5025dd58c0e1c
Mickaël Canévet <mickael.canevet@gmail.com>
2020-10-01 20:52:49 +0200

    Automate Vault configuration

--- scripts/configure-vault.sh
+++ scripts/configure-vault.sh
@@ -1,5 +1,20 @@
 #!/bin/sh -xe
 
+wget https://storage.googleapis.com/kubernetes-release/release/v1.19.0/bin/linux/amd64/kubectl -O ~/kubectl
+chmod +x ~/kubectl
+
+while ! ~/kubectl get ns vault; do
+	echo Waiting for vault namespace
+	sleep 15
+done
+
+while test "$(~/kubectl -n vault get pods --selector 'app.kubernetes.io/name=vault' --output=name | wc -l)" -eq 0; do
+	echo Waiting for pods in vault namespace
+	sleep 15
+done
+
+~/kubectl -n vault wait "$(~/kubectl -n vault get pods --selector 'app.kubernetes.io/name=vault' --output=name)" --for=condition=Ready --timeout=-1s
+
 cd vault || exit
 terraform init -upgrade
 terraform workspace select "$CLUSTER_NAME" || terraform workspace new "$CLUSTER_NAME"
