a984bb8a553992d16d2ccc28a59916a5b66eec04
Mickaël Canévet <mickael.canevet@gmail.com>
2020-10-01 22:04:35 +0200

    Improve Vault configuration

--- vault/main.tf
+++ vault/main.tf
@@ -1,3 +1,23 @@
+locals {
+  context = yamldecode(file(format("../terraform/terraform.tfstate.d/%s/kubeconfig.yaml", terraform.workspace)))
+  cluster = lookup(lookup(local.context, "clusters")[0], "cluster")
+  user    = lookup(lookup(local.context, "users")[0], "user")
+}
+
+provider "kubernetes" {
+  host                   = lookup(local.cluster, "server")
+  client_certificate     = base64decode(lookup(local.user, "client-certificate-data"))
+  client_key             = base64decode(lookup(local.user, "client-key-data"))
+  cluster_ca_certificate = base64decode(lookup(local.cluster, "certificate-authority-data"))
+}
+
+data "kubernetes_pod" "vault_0" {
+  metadata {
+    name      = "vault-0"
+    namespace = "vault"
+  }
+}
+
 provider "vault" {
   token           = "root"
   skip_tls_verify = true
@@ -8,12 +28,7 @@ resource "vault_auth_backend" "kubernetes" {
 }
 
 resource "vault_kubernetes_auth_backend_config" "in_cluster" {
-  backend         = vault_auth_backend.kubernetes.path
-  kubernetes_host = "https://kubernetes.default.svc"
-
-  lifecycle {
-    ignore_changes = [
-      kubernetes_ca_cert,
-    ]
-  }
+  backend            = vault_auth_backend.kubernetes.path
+  kubernetes_host    = lookup(local.cluster, "server")
+  kubernetes_ca_cert = base64decode(lookup(local.cluster, "certificate-authority-data"))
 }
