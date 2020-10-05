d4a54b6ae5c52bb6dd34db8bb33f6e223c60d6f2
Mickaël Canévet <mickael.canevet@gmail.com>
2020-10-05 07:43:52 +0200

    Test more URLs

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,9 @@
 # ArgoCD
 curl -k -I "https://argocd.apps.$BASE_DOMAIN"
 
+# Vault
+curl -k -I "https://vault.apps.$BASE_DOMAIN"
+
 # Prometheus
 curl -k -I "https://prometheus.apps.$BASE_DOMAIN"
 
@@ -11,3 +14,6 @@ curl -k -I "https://alertmanager.apps.$BASE_DOMAIN"
 
 # Grafana
 curl -k -I "https://grafana.apps.$BASE_DOMAIN"
+
+# Demo-app
+curl -k -I "https://demo-app.apps.$BASE_DOMAIN"
