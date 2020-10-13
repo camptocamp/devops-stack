226e02d347d788e567798a8929ba5b7686531c38
Mickaël Canévet <mickael.canevet@gmail.com>
2020-10-13 12:12:16 +0200

    Use absolute path for ARTIFACTS_DIR

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -11,13 +11,13 @@ terraform apply --auto-approve
 terraform plan --detailed-exitcode
 
 # Create values.yaml for ArgoCD app of apps
-terraform show --json > "../$ARTIFACTS_DIR/terraform.tfstate.json"
-cat << EOF > "../$ARTIFACTS_DIR/values.yaml"
+terraform show --json > "$ARTIFACTS_DIR/terraform.tfstate.json"
+cat << EOF > "$ARTIFACTS_DIR/values.yaml"
 ---
 spec:
   source:
     repoURL: $REPO_URL
     targetRevision: $CLUSTER_NAME
 
-baseDomain: $(~/jq -r '.values.root_module.resources[]|select(.type=="docker_container" and .name=="k3s_server").values.ip_address|gsub("\\.";"-") + ".nip.io"' "../$ARTIFACTS_DIR/terraform.tfstate.json")
+baseDomain: $(~/jq -r '.values.root_module.resources[]|select(.type=="docker_container" and .name=="k3s_server").values.ip_address|gsub("\\.";"-") + ".nip.io"' "$ARTIFACTS_DIR/terraform.tfstate.json")
 EOF
