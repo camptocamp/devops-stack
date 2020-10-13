0751e57e2c8f4750954f8e9d976a2c8c61f7ceeb
Mickaël Canévet <mickael.canevet@gmail.com>
2020-10-13 10:43:43 +0200

    Pin docker provider version

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -5,7 +5,8 @@ terraform {
       version = "2.3.0"
     }
     docker = {
-      source = "terraform-providers/docker"
+      source  = "terraform-providers/docker"
+      version = "2.7.2"
     }
   }
   required_version = ">= 0.13"
