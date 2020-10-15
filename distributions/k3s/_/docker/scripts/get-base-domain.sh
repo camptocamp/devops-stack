c71ec0caea3619a2d3474319dc811d2a43e04f57
Mickaël Canévet <mickael.canevet@gmail.com>
2020-10-15 17:16:40 +0200

    Use terraform.tfstate instead of terraform.tfstate.json

--- distributions/k3s/_/docker/scripts/get-base-domain.sh
+++ distributions/k3s/_/docker/scripts/get-base-domain.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -xe
 
-API_IP_ADDRESS=$(jq -r '.values.root_module.resources[]|select(.type=="docker_container" and .name=="k3s_server").values.ip_address' "$ARTIFACTS_DIR/terraform.tfstate.json")
+API_IP_ADDRESS=$(jq -r '.resources[]|select(.type=="docker_container" and .name=="k3s_server").instances[0].attributes.ip_address' "$ARTIFACTS_DIR/terraform.tfstate")
 echo "$(echo "$API_IP_ADDRESS"|tr '.' '-').nip.io"
--- distributions/k3s/_/docker/scripts/get-kubeconfig.sh
+++ distributions/k3s/_/docker/scripts/get-kubeconfig.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -xe
 
-API_IP_ADDRESS=$(jq -r '.values.root_module.resources[]|select(.type=="docker_container" and .name=="k3s_server").values.ip_address' "$ARTIFACTS_DIR/terraform.tfstate.json")
+API_IP_ADDRESS=$(jq -r '.resources[]|select(.type=="docker_container" and .name=="k3s_server").instances[0].attributes.ip_address' "$ARTIFACTS_DIR/terraform.tfstate")
 docker cp "k3s-server-$CLUSTER_NAME:/etc/rancher/k3s/k3s.yaml" "$ARTIFACTS_DIR/kubeconfig.yaml"
 sed -i -e "s/127.0.0.1/$API_IP_ADDRESS/" "$ARTIFACTS_DIR/kubeconfig.yaml"
