90b09e500853c0c79f32d4870b5d59473ddb6bd1
Mickaël Canévet <mickael.canevet@gmail.com>
2020-10-15 17:16:40 +0200

    Use terraform.tfstate instead of terraform.tfstate.json

--- distributions/k3s/_/docker/scripts/provision-after-script.sh
+++ distributions/k3s/_/docker/scripts/provision-after-script.sh
@@ -4,10 +4,6 @@ wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -O ~/jq
 chmod +x ~/jq
 
 # Create values.yaml for ArgoCD app of apps
-cd distributions/"$DISTRIBUTION"/terraform || exit
-terraform show --json > "$ARTIFACTS_DIR"/terraform.tfstate.json
-cd -
-
 cat << EOF > "$ARTIFACTS_DIR/values.yaml"
 ---
 spec:
@@ -15,5 +11,5 @@ spec:
     repoURL: $REPO_URL
     targetRevision: $CLUSTER_NAME
 
-baseDomain: $(~/jq -r '.values.root_module.resources[]|select(.type=="docker_container" and .name=="k3s_server").values.ip_address|gsub("\\.";"-") + ".nip.io"' "$ARTIFACTS_DIR/terraform.tfstate.json")
+baseDomain: $(~/jq -r '.resources[]|select(.type=="docker_container" and .name=="k3s_server").instances[0].attributes.ip_address|gsub("\\.";"-") + ".nip.io"' "$ARTIFACTS_DIR/terraform.tfstate")
 EOF
