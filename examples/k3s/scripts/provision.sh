8277f49a87340f9c24b5b452fdb2c3b3119318ad
Mickaël Canévet <mickael.canevet@gmail.com>
2020-10-27 23:18:41 +0100

    Allow to instanciate the devops stack

---
+++ examples/k3s/scripts/provision.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -xe
+
+mkdir -p ".terraform.d/plugin-cache"
+echo plugin_cache_dir = \""$PWD/.terraform.d/plugin-cache"\" > "$HOME/.terraformrc"
+
+# Terraform helm provider requires this file to be present
+mkdir "$HOME/.kube"
+touch "$HOME/.kube/config"
+
+mkdir -p bin
+export PATH="$PWD/bin:$PATH"
+
+if ! test -x bin/kubectl; then
+	wget https://storage.googleapis.com/kubernetes-release/release/v1.19.0/bin/linux/amd64/kubectl -O bin/kubectl
+	chmod +x bin/kubectl
+fi
+
+cd terraform || exit
+terraform init -upgrade
+terraform workspace select "$CLUSTER_NAME" || terraform workspace new "$CLUSTER_NAME"
+terraform init -upgrade
+terraform apply --auto-approve -target module.cluster.module.cluster
+terraform apply --auto-approve -target module.cluster.helm_release.argocd
+terraform apply --auto-approve
+terraform plan --detailed-exitcode
+cd - || exit
