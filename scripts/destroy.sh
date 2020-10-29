68f8c651bce8b46f30d8a95341399fc35cbabeb8
Mickaël Canévet <mickael.canevet@gmail.com>
2020-10-29 16:16:43 +0100

    Fix scripts

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -xe
 
+# Terraform helm provider requires this file to be present
+test -d "$HOME/.kube" || mkdir "$HOME/.kube"
+test -f "$HOME/.kube/config" || touch "$HOME/.kube/config"
+
 cd terraform || exit
 terraform init -upgrade
 terraform workspace select "$CLUSTER_NAME" || terraform workspace new "$CLUSTER_NAME"
