1a4f9631f1c28029e22d479e29e14eddab00f297
Mickaël Canévet <mickael.canevet@camptocamp.com>
2020-11-04 08:09:48 +0100

    Update destroy.sh

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -8,7 +8,7 @@ cd terraform || exit
 terraform init -upgrade
 terraform workspace select "$CLUSTER_NAME" || terraform workspace new "$CLUSTER_NAME"
 terraform init -upgrade
-terraform destroy --auto-approve -target module.cluster.module.cluster
+terraform destroy --auto-approve
 if [ "$CLUSTER_NAME" != "default" ]; then
 	terraform workspace select default
 	terraform workspace delete "$CLUSTER_NAME"
