b616b30cf02b87d1c3f863013ee0c4a713ad46ef
Raphaël Pinson <raphael.pinson@camptocamp.com>
2020-11-13 14:00:05 +0100

    Add OAuth to Grafana (#230)
    * Fix Grafana with Prometheus (no proxy)
    
    * Add oauth to grafana

--- modules/eks-aws/cognito.tf
+++ modules/eks-aws/cognito.tf
@@ -23,7 +23,7 @@ resource "aws_cognito_user_pool_client" "client" {
 
   callback_urls = [
     format("https://argocd.apps.%s.%s/auth/callback", var.cluster_name, var.base_domain),
-    format("https://grafana.apps.%s.%s/auth/callback", var.cluster_name, var.base_domain),
+    format("https://grafana.apps.%s.%s/login/generic_oauth", var.cluster_name, var.base_domain),
     format("https://prometheus.apps.%s.%s/oauth2/callback", var.cluster_name, var.base_domain),
     format("https://alertmanager.apps.%s.%s/oauth2/callback", var.cluster_name, var.base_domain),
   ]
