143616592ef027e51f0c68204da6aef3d5edcc06
Mickaël Canévet <mickael.canevet@gmail.com>
2020-11-13 08:18:21 +0100

    Make pipeline less verbose

--- scripts/app-diff.sh
+++ scripts/app-diff.sh
@@ -1,11 +1,8 @@
 #!/bin/sh -xe
 
-argocd app list -owide
-
 for app_dir in ../../argocd/*;
 do
 	app=${app_dir#../../argocd/}
-	app=${app%*/}
 	test -f "$app_dir/Chart.yaml" && helm dependency update "$app_dir"
 	argocd app diff "$app" --local "$app_dir" || true
 done
