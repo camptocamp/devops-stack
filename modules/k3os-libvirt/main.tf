14580542392b102f752c063d8db97817d0a37f96
Mickaël Canévet <mickael.canevet@gmail.com>
2020-11-22 09:33:24 +0100

    Deploy ArgoCD using argo-helm module

--- modules/k3os-libvirt/main.tf
+++ modules/k3os-libvirt/main.tf
@@ -29,16 +29,8 @@ module "cluster" {
   agent_memory  = 2048
 }
 
-resource "helm_release" "argocd" {
-  name              = "argocd"
-  chart             = "${path.module}/../../argocd/argocd"
-  namespace         = "argocd"
-  dependency_update = true
-  create_namespace  = true
-
-  values = [
-    file("${path.module}/../../argocd/argocd/values.yaml")
-  ]
+module "argocd" {
+  source = "../argocd-helm"
 
   depends_on = [
     module.cluster,
