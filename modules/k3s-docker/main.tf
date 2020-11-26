723bd82ce227e02e8925b56e30d6b4edfacd6226
Raphaël Pinson <raphael.pinson@camptocamp.com>
2020-11-26 09:11:33 +0100

    Disable metrics-server in k3s
    Signed-off-by: Raphaël Pinson <raphael.pinson@camptocamp.com>

--- modules/k3s-docker/main.tf
+++ modules/k3s-docker/main.tf
@@ -29,9 +29,13 @@ module "cluster" {
   source  = "camptocamp/k3s/docker"
   version = "0.3.2"
 
-  cluster_name = var.cluster_name
-  k3s_version  = var.k3s_version
-  node_count   = var.node_count
+  cluster_name  = var.cluster_name
+  k3s_version   = var.k3s_version
+  node_count    = var.node_count
+  server_config = [
+    "--disable", "traefik",
+    "--disable", "metrics-server",
+  ]
 }
 
 module "argocd" {
