f3d1c6ff682bc3d90518d2587684b241610ea915
Mickaël Canévet <mickael.canevet@gmail.com>
2020-11-26 10:39:11 +0100

    Mutualize most configuration

--- modules/eks-aws/main.tf
+++ modules/eks-aws/main.tf
@@ -146,6 +146,18 @@ resource "helm_release" "app_of_apps" {
         target_revision                 = var.target_revision
         argocd_accounts_pipeline_tokens = module.argocd.argocd_accounts_pipeline_tokens
         extra_apps                      = var.extra_apps
+        cluster_name                    = var.cluster_name
+        base_domain                     = var.base_domain
+        cluster_issuer                  = "letsencrypt-prod"
+        client_id                       = aws_cognito_user_pool_client.client.id
+        client_secret                   = aws_cognito_user_pool_client.client.client_secret
+        admin_password                  = ""
+        minio_access_key                = ""
+        minio_secret_key                = ""
+        enable_efs                      = var.enable_efs
+        enable_keycloak                 = false
+        enable_olm                      = false
+        enable_minio                    = false
       }
     ),
     templatefile("${path.module}/values.tmpl.yaml",
@@ -156,12 +168,13 @@ resource "helm_release" "app_of_apps" {
         cert_manager_assumable_role_arn = module.iam_assumable_role_cert_manager.this_iam_role_arn,
         loki_assumable_role_arn         = module.iam_assumable_role_loki.this_iam_role_arn,
         loki_bucket_name                = aws_s3_bucket.loki.id,
-        cognito_user_pool_id            = var.cognito_user_pool_id
-        cognito_user_pool_domain        = var.cognito_user_pool_domain
-        cognito_user_pool_client_id     = aws_cognito_user_pool_client.client.id
-        cognito_user_pool_client_secret = aws_cognito_user_pool_client.client.client_secret
+        oidc_issuer_url                 = format("https://cognito-idp.%s.amazonaws.com/%s", data.aws_region.current.name, var.cognito_user_pool_id)
+        oauth2_oauth_url                = format("https://%s.auth.%s.amazoncognito.com/oauth2/authorize", var.cognito_user_pool_domain, data.aws_region.current.name)
+        oauth2_token_url                = format("https://%s.auth.%s.amazoncognito.com/oauth2/token", var.cognito_user_pool_domain, data.aws_region.current.name)
+        oauth2_api_url                  = format("https://%s.auth.%s.amazoncognito.com/oauth2/userInfo", var.cognito_user_pool_domain, data.aws_region.current.name)
+        client_id                       = aws_cognito_user_pool_client.client.id
+        client_secret                   = aws_cognito_user_pool_client.client.client_secret
         cookie_secret                   = random_password.oauth2_cookie_secret.result
-        enable_efs                      = var.enable_efs
         efs_filesystem_id               = var.enable_efs ? module.efs.0.this_efs_mount_target_file_system_id : ""
         efs_dns_name                    = var.enable_efs ? module.efs.0.this_efs_mount_target_full_dns_name : ""
       }
