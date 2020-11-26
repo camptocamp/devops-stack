e5c913a3061452601daf12f00fbc215dc18be330
Mickaël Canévet <mickael.canevet@gmail.com>
2020-11-26 11:30:25 +0100

    Configure Applications to use Keycloak

--- modules/k3s-docker/main.tf
+++ modules/k3s-docker/main.tf
@@ -59,8 +59,13 @@ resource "helm_release" "app_of_apps" {
         cluster_name                    = var.cluster_name
         base_domain                     = local.base_domain
         cluster_issuer                  = "selfsigned-issuer"
+        oidc_issuer_url                 = format("https://keycloak.apps.%s/auth/realms/kubernetes", local.base_domain)
+        oauth2_oauth_url                = format("https://keycloak.apps.%s/auth/realms/kubernetes/protocol/openid-connect/auth", local.base_domain)
+        oauth2_token_url                = format("https://keycloak.apps.%s/auth/realms/kubernetes/protocol/openid-connect/token", local.base_domain)
+        oauth2_api_url                  = format("https://keycloak.apps.%s/auth/realms/kubernetes/protocol/openid-connect/userinfo", local.base_domain)
         client_id                       = "applications"
         client_secret                   = random_password.clientsecret.result
+        cookie_secret                   = random_password.oauth2_cookie_secret.result
         admin_password                  = random_password.admin_password.result
         minio_access_key                = var.enable_minio ? random_password.minio_accesskey.0.result : ""
         minio_secret_key                = var.enable_minio ? random_password.minio_secretkey.0.result : ""
@@ -68,6 +73,15 @@ resource "helm_release" "app_of_apps" {
         enable_keycloak                 = true
         enable_olm                      = true
         enable_minio                    = var.enable_minio
+
+        oauth2_proxy_extra_args = [
+          "--insecure-oidc-skip-issuer-verification=true",
+          "--ssl-insecure-skip-verify=true",
+        ]
+
+        grafana_generic_oauth_extra_args = {
+          tls_skip_verify_insecure = true
+        }
       }
     ),
     templatefile("${path.module}/values.tmpl.yaml",
@@ -92,7 +106,7 @@ resource "random_password" "admin_password" {
   special = false
 }
 
-resource "random_password" "cookie_secret" {
+resource "random_password" "oauth2_cookie_secret" {
   length  = 16
   special = false
 }
