a6f435fed35cf29b1167f44eba920756155c6e83
Raphaël Pinson <raphael.pinson@camptocamp.com>
2020-12-03 11:38:14 +0100

    Remove deprecated argocd_accounts_pipeline_tokens output

--- modules/argocd-helm/outputs.tf
+++ modules/argocd-helm/outputs.tf
@@ -1,8 +1,3 @@
-output "argocd_accounts_pipeline_tokens" {
-  description = "The token created for the pipeline."
-  value       = local.argocd_accounts_pipeline_tokens
-}
-
 output "argocd_auth_token" {
   description = "The token to set in ARGOCD_AUTH_TOKEN environment variable."
   value       = jwt_hashed_token.argocd.token
