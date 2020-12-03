a9cbfae26c7f298fe74bbdd758cb0d1bfa2892fa
Raphaël Pinson <raphael.pinson@camptocamp.com>
2020-12-03 11:54:51 +0100

    app_of_apps_values output

--- modules/argocd-helm/outputs.tf
+++ modules/argocd-helm/outputs.tf
@@ -2,3 +2,8 @@ output "argocd_auth_token" {
   description = "The token to set in ARGOCD_AUTH_TOKEN environment variable."
   value       = jwt_hashed_token.argocd.token
 }
+
+output "app_of_apps_values" {
+  description = "App of Apps values"
+  value       = helm_release.app_of_apps.values
+}
