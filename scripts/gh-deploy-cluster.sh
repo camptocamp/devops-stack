34f76a7d9ff80a39402bce72204ab49e4641cd12
Mickaël Canévet <mickael.canevet@gmail.com>
2020-12-06 05:58:46 +0100

    Fix github workflow

--- scripts/gh-deploy-cluster.sh
+++ scripts/gh-deploy-cluster.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 FLAVOR="$1"
+REPO_URL="$GITHUB_SERVER_URL/$2/camptocamp-devops-stack.git"
+TARGET_REVISION="$3"
 
 mkdir -p "$HOME/bin"
 export PATH="$HOME/bin:$PATH"
@@ -22,7 +24,7 @@ ln -s "$GITHUB_WORKSPACE" "$RUNNER_WORKSPACE/camptocamp-devops-stack"
 cd "$RUNNER_WORKSPACE/camptocamp-devops-stack/tests/$FLAVOR" || exit
 
 export CLUSTER_NAME=default
-export TF_VAR_repo_url="$2"
-export TF_VAR_target_revision="$3"
+export TF_VAR_repo_url="$REPO_URL"
+export TF_VAR_target_revision="$TARGET_REVISION"
 
 ../../scripts/provision.sh
--- scripts/gh-plan.sh
+++ scripts/gh-plan.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 FLAVOR="$1"
+REPO_URL="$GITHUB_SERVER_URL/$2/camptocamp-devops-stack.git"
+TARGET_REVISION="$3"
 
 mkdir -p "$RUNNER_WORKSPACE"
 ln -s "$GITHUB_WORKSPACE" "$RUNNER_WORKSPACE/camptocamp-devops-stack"
 cd "$RUNNER_WORKSPACE/camptocamp-devops-stack/tests/$FLAVOR" || exit
 
 export CLUSTER_NAME=default
-export TF_VAR_repo_url="$2"
-export TF_VAR_target_revision="$3"
+export TF_VAR_repo_url="$REPO_URL"
+export TF_VAR_target_revision="$TARGET_REVISION"
 
 ../../scripts/plan.sh
