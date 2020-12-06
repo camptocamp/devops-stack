56abd43cf1404934ed6dc5f11cbd834b67a23e4b
Mickaël Canévet <mickael.canevet@gmail.com>
2020-12-06 06:15:49 +0100

    Store kubeconfig in a temporary file

--- scripts/app-diff.sh
+++ scripts/app-diff.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-python3 -c "import sys, json; print(json.load(sys.stdin)['kubeconfig']['value'])" < terraform/outputs.json > kubeconfig.yaml
-chmod 0600 kubeconfig.yaml
-export KUBECONFIG=kubeconfig.yaml
+KUBECONFIG=$(mktemp /tmp/kubeconfig.XXXXXX)
+export KUBECONFIG
+
+python3 -c "import sys, json; print(json.load(sys.stdin)['kubeconfig']['value'])" < terraform/outputs.json > "$KUBECONFIG"
+chmod 0600 "$KUBECONFIG"
 
 ARGOCD_AUTH_TOKEN=$(python3 -c "import sys, json; print(json.load(sys.stdin)['argocd_auth_token']['value'])" < terraform/outputs.json)
 export ARGOCD_AUTH_TOKEN
