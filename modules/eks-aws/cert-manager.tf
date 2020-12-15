ef23b740136c7e6440144441bec7dfb38f31a6fc
Mickaël Canévet <mickael.canevet@gmail.com>
2020-12-15 12:51:42 +0100

    Upgrade dependencies for eks-aws

--- modules/eks-aws/cert-manager.tf
+++ modules/eks-aws/cert-manager.tf
@@ -6,7 +6,7 @@ data "aws_region" "current" {}
 
 module "iam_assumable_role_cert_manager" {
   source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version                       = "3.3.0"
+  version                       = "3.6.0"
   create_role                   = true
   number_of_role_policy_arns    = 1
   role_name                     = format("cert-manager-%s", var.cluster_name)
