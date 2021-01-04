3de22a66a540787b5a1d4127ace6c8d17ce70dc1
Julien Acroute <julien.acroute@camptocamp.com>
2021-01-04 13:43:17 +0100

    Remove yq usage in custom diff tool

--- docker-argocd-app-diff/custom-diff-tool.sh
+++ docker-argocd-app-diff/custom-diff-tool.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
 
-kind=$(yq r $2 kind)
-apiVersion=$(yq r $2 apiVersion)
-name=$(yq r $2 metadata.name)
-namespace=$(yq r $2 metadata.namespace)
-parent_app=$(yq r $2 'metadata.labels."app.kubernetes.io/instance"')
-
-for file in $1 $2; do
-  yq d -i $file 'metadata.annotations."kubectl.kubernetes.io/last-applied-configuration"'
-  yq d -i $file 'metadata.managedFields'
-  yq d -i $file 'status'
-  yq d -i $file 'metadata.creationTimestamp'
-  yq d -i $file 'metadata.resourceVersion'
-  yq d -i $file 'metadata.selfLink'
-  yq d -i $file 'metadata.uid'
-done
-
 # Compute and format diff
 diff=$(mktemp)
-diff -u9999999 $* | tail -n +4 > $diff
+diff $* | tail -n +4 > $diff
 ret=${PIPESTATUS[0]}
 
 if [ $ret -eq 1 ]; then
@@ -27,13 +11,6 @@ if [ $ret -eq 1 ]; then
   echo '```'
 else
   echo "*No differences*" > /dev/null
-fi
-
-# If diff is about an argocd App we can add a link to app diff
-if [ "$apiVersion/$kind" = "argoproj.io/v1alpha1/Application" ]; then
-  if [ "$name" != "$parent_app" ]; then # Avoid endless loop
-    echo "Go to [$name diff](#$name)"
-  fi
 fi
 
 exit $ret
