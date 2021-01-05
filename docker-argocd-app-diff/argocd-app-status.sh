e90e03a615ba02278cb97770339b21bab32be3ac
Julien Acroute <julien.acroute@camptocamp.com>
2021-01-05 09:20:28 +0100

    Improve app status cache

--- docker-argocd-app-diff/argocd-app-status.sh
+++ docker-argocd-app-diff/argocd-app-status.sh
@@ -9,11 +9,15 @@ refresh_app_status () {
     name=$(echo $line | cut -f 1 -d ' ')
     sync_status=$(echo $line | cut -f 2 -d ' ')
     echo $sync_status > /tmp/app_status/$name
-  done < <(kubectl get Application --all-namespaces -o json | jq -r '.items[] | "\(.metadata.name) \(.status.sync.status)"')
+  done < <(argocd app list -o json 2> /dev/null | jq -r '.[] | "\(.metadata.name) \(.status.sync.status)"')
   [ "$DEBUG" = 'true' ] && echo "OK"
 
 }
 
 get_app_status () {
     cat /tmp/app_status/$1
+}
+
+update_app_status () {
+  argocd app get $1 -o json 2> /dev/null | jq -r '.status.sync.status' > /tmp/app_status/$1
 }
