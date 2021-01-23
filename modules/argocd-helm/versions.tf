b7fe103fb399a3c03145fe5793e6b08e3e131d3a
Mickaël Canévet <mickael.canevet@gmail.com>
2021-01-23 10:16:56 +0100

    Upgrade helm provider to 2.0.2

--- modules/argocd-helm/versions.tf
+++ modules/argocd-helm/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "2.0.1"
+      version = "~> 2.0"
     }
     jwt = {
       source  = "camptocamp/jwt"
