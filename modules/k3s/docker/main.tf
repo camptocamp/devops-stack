eaca2d47988dff903429147e5099f54bfc82337f
Mickaël Canévet <mickael.canevet@gmail.com>
2021-01-23 08:24:45 +0100

    Upgrade camptocamp/k3s/docker module to 0.7.1

--- modules/k3s/docker/main.tf
+++ modules/k3s/docker/main.tf
@@ -1,6 +1,6 @@
 module "cluster" {
   source  = "camptocamp/k3s/docker"
-  version = "0.6.0"
+  version = "0.7.1"
 
   network_name = "bridge"
   cluster_name = var.cluster_name
@@ -10,4 +10,20 @@ module "cluster" {
     "--disable", "traefik",
     "--disable", "metrics-server",
   ]
+
+  registry_mirrors = {
+    "docker.io" = [
+      "REGISTRY_PROXY_REMOTEURL=https://registry-1.docker.io",
+    ],
+    "quay.io" = [
+      "REGISTRY_PROXY_REMOTEURL=https://quay.io/repository",
+      "REGISTRY_COMPATIBILITY_SCHEMA1_ENABLED=true",
+    ],
+    "gcr.io" = [
+      "REGISTRY_PROXY_REMOTEURL=https://gcr.io",
+    ],
+    "us.gcr.io" = [
+      "REGISTRY_PROXY_REMOTEURL=https://us.gcr.io",
+    ],
+  }
 }
