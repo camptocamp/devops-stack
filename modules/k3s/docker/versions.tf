6d5248ca29a34fbe54e1eb9b669a39a396739143
Mickaël Canévet <mickael.canevet@gmail.com>
2021-01-23 08:28:59 +0100

    Upgrade docker provider to 2.11.0

--- modules/k3s/docker/versions.tf
+++ modules/k3s/docker/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "2.8.0"
+      version = "2.11.0"
     }
     random = {
       source  = "hashicorp/random"
