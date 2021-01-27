f3ae8f546ceb64598764a79d5999ee68f190141c
Mickaël Canévet <mickael.canevet@gmail.com>
2021-01-27 10:22:40 +0100

    AKS : Add missing providers pining

--- modules/aks/azure/versions.tf
+++ modules/aks/azure/versions.tf
@@ -5,10 +5,27 @@ terraform {
       version = "1.0.0"
     }
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
+      version = "2.34.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "1.13.3"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.0.2"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.0.0"
     }
     random = {
       source  = "hashicorp/random"
+      version = "3.0.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
       version = "3.0.0"
     }
   }
