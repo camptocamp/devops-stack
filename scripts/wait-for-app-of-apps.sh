8fe4dc13b9406d40cdb2bac6f2a0a0771b94f3f1
Mickaël Canévet <mickael.canevet@gmail.com>
2021-01-30 08:12:47 +0100

    Fix wait for app of apps script

--- scripts/wait-for-app-of-apps.sh
+++ scripts/wait-for-app-of-apps.sh
@@ -14,7 +14,7 @@ export ARGOCD_AUTH_TOKEN
 ARGOCD_OPTS="--plaintext --port-forward --port-forward-namespace argocd"
 export ARGOCD_OPTS
 
-while ! argocd app wait -l argocd.argoproj.io/instance=apps --sync --health --timeout 30
+while ! argocd app wait apps --sync --health --timeout 30
 do
 	argocd app list -owide || true
 done
