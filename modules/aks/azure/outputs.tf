b31f955939c8d97c42c80d9fa6858c754b8f5a71
Mickaël Canévet <mickael.canevet@gmail.com>
2021-02-03 14:55:21 +0100

    Add output for prometheus identity

--- modules/aks/azure/outputs.tf
+++ modules/aks/azure/outputs.tf
@@ -1,3 +1,7 @@
 output "node_resource_group" {
   value = module.cluster.node_resource_group
 }
+
+output "prometheus_user_assigned_identity_principal_id" {
+  value = azurerm_user_assigned_identity.kube_prometheus_stack_prometheus.principal_id
+}
