fbea1a0461eb56c776f61a17f6e397b1b85b1076
Mickaël Canévet <mickael.canevet@gmail.com>
2021-02-09 16:34:29 +0100

    Refactor CI scripts

--- .github/scripts/gh-deploy-cluster.sh
+++ .github/scripts/gh-deploy-cluster.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-REPO_URL="$GITHUB_SERVER_URL/$1/camptocamp-devops-stack.git"
-TARGET_REVISION="$2"
-
 # This hack is mendatory because we are using mount binds with absolute path in docker containers
 mkdir -p "$RUNNER_WORKSPACE"
 ln -s "$GITHUB_WORKSPACE" "$RUNNER_WORKSPACE/camptocamp-devops-stack"
 
 export TF_ROOT="$RUNNER_WORKSPACE/camptocamp-devops-stack/$TF_ROOT" || exit
-export CLUSTER_NAME=default
-export TF_VAR_repo_url="$REPO_URL"
-export TF_VAR_target_revision="$TARGET_REVISION"
 
 ./scripts/provision.sh
