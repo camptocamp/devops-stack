509af3fc685b3c58e7811621a0e46f9303f529bb
Mickaël Canévet <mickael.canevet@gmail.com>
2021-02-09 14:02:33 +0100

    Move jq and helm installation in provision.sh

--- .github/scripts/gh-deploy-cluster.sh
+++ .github/scripts/gh-deploy-cluster.sh
@@ -4,15 +4,6 @@ FLAVOR="$1"
 REPO_URL="$GITHUB_SERVER_URL/$2/camptocamp-devops-stack.git"
 TARGET_REVISION="$3"
 
-mkdir -p "$HOME/bin"
-export PATH="$HOME/bin:$PATH"
-
-wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -O "$HOME/bin/jq"
-chmod +x "$HOME/bin/jq"
-
-wget https://get.helm.sh/helm-v3.4.0-linux-amd64.tar.gz -O - | tar xz linux-amd64/helm -O > "$HOME/bin/helm"
-chmod +x "$HOME/bin/helm"
-
 # This hack is mendatory because we are using mount binds with absolute path in docker containers
 mkdir -p "$RUNNER_WORKSPACE"
 ln -s "$GITHUB_WORKSPACE" "$RUNNER_WORKSPACE/camptocamp-devops-stack"
