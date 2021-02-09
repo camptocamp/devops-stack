235ab675d17f361a4c199999d2100d77754812a5
Mickaël Canévet <mickael.canevet@gmail.com>
2021-02-09 13:55:31 +0100

    Remove debug

--- .github/scripts/gh-deploy-cluster.sh
+++ .github/scripts/gh-deploy-cluster.sh
@@ -13,11 +13,6 @@ chmod +x "$HOME/bin/jq"
 wget https://get.helm.sh/helm-v3.4.0-linux-amd64.tar.gz -O - | tar xz linux-amd64/helm -O > "$HOME/bin/helm"
 chmod +x "$HOME/bin/helm"
 
-env|grep GITHUB_
-env|grep ACTIONS_
-env|grep RUNNER_
-env|grep INPUT_
-
 # This hack is mendatory because we are using mount binds with absolute path in docker containers
 mkdir -p "$RUNNER_WORKSPACE"
 ln -s "$GITHUB_WORKSPACE" "$RUNNER_WORKSPACE/camptocamp-devops-stack"
