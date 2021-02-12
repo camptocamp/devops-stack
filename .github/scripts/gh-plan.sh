28f3959ea2ecec04ae7a8623cfdd5cb18707514a
Mickaël Canévet <mickael.canevet@gmail.com>
2021-02-12 10:33:46 +0100

    Use hashicorp/setup-terraform action in github workflow

--- .github/scripts/gh-plan.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# This hack is mendatory because we are using mount binds with absolute path in docker containers
-mkdir -p "$RUNNER_WORKSPACE"
-ln -s "$GITHUB_WORKSPACE" "$RUNNER_WORKSPACE/camptocamp-devops-stack"
-
-export TF_ROOT="$RUNNER_WORKSPACE/camptocamp-devops-stack/$TF_ROOT"
-
-./scripts/plan.sh
