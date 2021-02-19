ffa962a70d0670190b42dfcb637536bde00f8a25
Mickaël Canévet <mickael.canevet@gmail.com>
2021-02-19 13:28:07 +0100

    Don't use scripts in pipelines

--- scripts/wait-for-app-of-apps.sh
+++ scripts/wait-for-app-of-apps.sh
@@ -2,16 +2,12 @@
 
 set -e
 
-TF_ROOT="${TF_ROOT:-terraform}"
+pwd
+echo HOME="$HOME"
 
-KUBECONFIG=$(mktemp /tmp/kubeconfig.XXXXXX)
-export KUBECONFIG
+echo "$ARGOCD_AUTH_TOKEN"
 
-python3 -c "import sys, json; print(json.load(sys.stdin)['kubeconfig']['value'])" < "$TF_ROOT/outputs.json" > "$KUBECONFIG"
-chmod 0600 "$KUBECONFIG"
-
-ARGOCD_AUTH_TOKEN=$(python3 -c "import sys, json; print(json.load(sys.stdin)['argocd_auth_token']['value'])" < "$TF_ROOT/outputs.json")
-export ARGOCD_AUTH_TOKEN
+cat ~/.kube/config
 
 ARGOCD_OPTS="--plaintext --port-forward --port-forward-namespace argocd"
 export ARGOCD_OPTS
@@ -20,5 +16,3 @@ while ! argocd app wait apps --sync --health --timeout 30
 do
 	argocd app list -owide || true
 done
-
-rm "$KUBECONFIG"
