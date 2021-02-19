f2a6284059fbdfc9fe94df1e23e900b73fea23ee
Mickaël Canévet <mickael.canevet@gmail.com>
2021-02-19 07:08:10 +0100

    Don't clone repo in app-diff.sh

--- scripts/app-diff.sh
+++ scripts/app-diff.sh
@@ -13,9 +13,6 @@ chmod 0600 "$KUBECONFIG"
 ARGOCD_AUTH_TOKEN=$(python3 -c "import sys, json; print(json.load(sys.stdin)['argocd_auth_token']['value'])" < "$TF_ROOT/outputs.json")
 export ARGOCD_AUTH_TOKEN
 
-REPO_URL=$(python3 -c "import sys, json; print(json.load(sys.stdin)['repo_url']['value'])" < "$TF_ROOT/outputs.json")
-TARGET_REVISION=$(python3 -c "import sys, json; print(json.load(sys.stdin)['target_revision']['value'])" < "$TF_ROOT/outputs.json")
-
 # FIXME: find a more robust way to do this
 APP_OF_APPS_VALUES_0=$(python3 -c "import sys, json; print(json.load(sys.stdin)['app_of_apps_values']['value'][0])" < "$TF_ROOT/outputs.json")
 APP_OF_APPS_VALUES_1=$(python3 -c "import sys, json; print(json.load(sys.stdin)['app_of_apps_values']['value'][1])" < "$TF_ROOT/outputs.json")
@@ -23,15 +20,6 @@ APP_OF_APPS_VALUES_2=$(python3 -c "import sys, json; print(json.load(sys.stdin)[
 
 export KUBECTL_EXTERNAL_DIFF="diff -u"
 export ARGOCD_OPTS="--plaintext --port-forward --port-forward-namespace argocd"
-
-cd /tmp || exit
-
-git clone "$REPO_URL"
-cd camptocamp-devops-stack || exit
-git checkout "$TARGET_REVISION"
-cd - || exit
-
-helm version
 
 echo Update app of apps without syncPolicy
 date
