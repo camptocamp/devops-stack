212d5daad12a3c2a2ba9989af2215901b71c56f6
Mickaël Canévet <mickael.canevet@gmail.com>
2021-02-23 10:06:33 +0100

    Upgrade hashicorp/azurerm provider to 2.48.0

--- modules/aks/azure/versions.tf
+++ modules/aks/azure/versions.tf
@@ -6,7 +6,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.34.0"
+      version = "2.48.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
