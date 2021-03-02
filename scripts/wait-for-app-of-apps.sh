60dff60f00169bdc6d771abec8483c6c17b6e010
Mickaël Canévet <mickael.canevet@gmail.com>
2021-03-02 18:31:07 +0100

    Get rid of outputs.json

--- scripts/wait-for-app-of-apps.sh
+++ scripts/wait-for-app-of-apps.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
+# This script requires this environment variables to be set:
+#
+# - KUBECONFIG: pointing to a file containing the Kubernetes context to use as we use port-forwarding
+# - ARGOCD_AUTH_TOKEN: a read-only token that is allowed to perform argocd app list and argocd app diff
+
 set -e
 
-TF_ROOT="${TF_ROOT:-terraform}"
-
-KUBECONFIG=$(mktemp /tmp/kubeconfig.XXXXXX)
-export KUBECONFIG
-
-python3 -c "import sys, json; print(json.load(sys.stdin)['kubeconfig']['value'])" < "$TF_ROOT/outputs.json" > "$KUBECONFIG"
-chmod 0600 "$KUBECONFIG"
-
-ARGOCD_AUTH_TOKEN=$(python3 -c "import sys, json; print(json.load(sys.stdin)['argocd_auth_token']['value'])" < "$TF_ROOT/outputs.json")
-export ARGOCD_AUTH_TOKEN
-
-ARGOCD_OPTS="--plaintext --port-forward --port-forward-namespace argocd"
-export ARGOCD_OPTS
+export ARGOCD_OPTS="--plaintext --port-forward --port-forward-namespace argocd"
 
 while ! argocd app wait apps --sync --health --timeout 30
 do
 	argocd app list -owide || true
 done
-
-rm "$KUBECONFIG"
