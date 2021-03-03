67973851b213ffee1867481c87c8159180bd127e
Mickaël Canévet <mickael.canevet@gmail.com>
2021-03-03 09:35:14 +0100

    Release 0.27.0

--- modules/variables.tf
+++ modules/variables.tf
@@ -12,7 +12,7 @@ variable "repo_url" {
 variable "target_revision" {
   description = "The source target revision of ArgoCD's app of apps."
   type        = string
-  default     = "v0.26.0"
+  default     = "v0.27.0"
 }
 
 variable "app_of_apps_values_overrides" {
