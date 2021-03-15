7ebdc86f509aefc7a956d5ff9682cbdaed42259c
Mickaël Canévet <mickael.canevet@gmail.com>
2021-03-15 17:14:45 +0100

    Fix regression

--- modules/eks/aws/main.tf
+++ modules/eks/aws/main.tf
@@ -115,12 +115,13 @@ module "argocd" {
 
   cluster_issuer = "letsencrypt-prod"
   oidc = var.oidc != null ? var.oidc : {
-    issuer_url    = format("https://cognito-idp.%s.amazonaws.com/%s", data.aws_region.current.name, var.cognito_user_pool_id)
-    oauth_url     = format("https://%s.auth.%s.amazoncognito.com/oauth2/authorize", var.cognito_user_pool_domain, data.aws_region.current.name)
-    token_url     = format("https://%s.auth.%s.amazoncognito.com/oauth2/token", var.cognito_user_pool_domain, data.aws_region.current.name)
-    api_url       = format("https://%s.auth.%s.amazoncognito.com/oauth2/userInfo", var.cognito_user_pool_domain, data.aws_region.current.name)
-    client_id     = aws_cognito_user_pool_client.client.0.id
-    client_secret = aws_cognito_user_pool_client.client.0.client_secret
+    issuer_url              = format("https://cognito-idp.%s.amazonaws.com/%s", data.aws_region.current.name, var.cognito_user_pool_id)
+    oauth_url               = format("https://%s.auth.%s.amazoncognito.com/oauth2/authorize", var.cognito_user_pool_domain, data.aws_region.current.name)
+    token_url               = format("https://%s.auth.%s.amazoncognito.com/oauth2/token", var.cognito_user_pool_domain, data.aws_region.current.name)
+    api_url                 = format("https://%s.auth.%s.amazoncognito.com/oauth2/userInfo", var.cognito_user_pool_domain, data.aws_region.current.name)
+    client_id               = aws_cognito_user_pool_client.client.0.id
+    client_secret           = aws_cognito_user_pool_client.client.0.client_secret
+    oauth2_proxy_extra_args = []
   }
   loki = {
     bucket_name = aws_s3_bucket.loki.id,
