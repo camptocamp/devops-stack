32fa55094ac147bf14aec524b07fa48a0a431089
Raphaël Pinson <raphael.pinson@camptocamp.com>
2021-03-22 15:06:48 +0100

    Add outputs to k3s-libvirt example
    Signed-off-by: Raphaël Pinson <raphael.pinson@camptocamp.com>

---
+++ examples/k3s-libvirt-demo-app/terraform/outputs.tf
@@ -0,0 +1,33 @@
+# terraform/outputs.tf
+
+output "argocd_auth_token" {
+  sensitive = true
+  value     = module.cluster.argocd_auth_token
+}
+
+output "kubeconfig" {
+  sensitive = true
+  value     = module.cluster.kubeconfig
+}
+
+output "argocd_server" {
+  value = module.cluster.argocd_server
+}
+
+output "repo_url" {
+  value = module.cluster.repo_url
+}
+
+output "target_revision" {
+  value = module.cluster.target_revision
+}
+
+output "app_of_apps_values" {
+  sensitive = true
+  value     = module.cluster.app_of_apps_values
+}
+
+output "admin_password" {
+  sensitive = true
+  value     = module.cluster.admin_password
+}
