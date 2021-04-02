8856102798528f14a12ca4b93fc9954fc298593d
Mickaël Canévet <mickael.canevet@gmail.com>
2021-04-02 08:54:05 +0200

    Remove unused outputs

--- modules/k3s/outputs.tf
+++ modules/k3s/outputs.tf
@@ -2,23 +2,6 @@ output "base_domain" {
   value = local.base_domain
 }
 
-output "kubernetes_host" {
-  value = local.kubernetes_host
-}
-
-output "kubernetes_username" {
-  value = local.kubernetes_username
-}
-
-output "kubernetes_password" {
-  value     = local.kubernetes_password
-  sensitive = true
-}
-
-output "kubernetes_cluster_ca_certificate" {
-  value = local.kubernetes_cluster_ca_certificate
-}
-
 output "admin_password" {
   value     = random_password.admin_password.result
   sensitive = true
