b1e6ac152ac619f37116397401941c74af3a2911
Mickaël Canévet <mickael.canevet@gmail.com>
2021-04-21 15:12:38 +0200

    Don't clone repo in integration tests
    This is not required anymore

--- .github/scripts/app-diff.sh
+++ .github/scripts/app-diff.sh
@@ -5,8 +5,6 @@
 # - KUBECONFIG: pointing to a file containing the Kubernetes context to use as we use port-forwarding
 # - ARGOCD_SERVER: The URL of the ArgoCD server
 # - ARGOCD_AUTH_TOKEN: a read-only token that is allowed to perform argocd app list and argocd app diff
-# - REPO_URL: the repoUrl of the destination DevOps stack
-# - TARGET_REVISION: the targetRevision of the destination DevOps Stack
 #
 # It also requires 2 files ro be present (TODO: find a better approach for this):
 # - values0.yaml
@@ -19,15 +17,8 @@ export KUBECTL_EXTERNAL_DIFF="diff -u"
 # TODO: find a way to disable this flags
 export ARGOCD_OPTS="--insecure --grpc-web"
 
-cd /tmp || exit
-
-git clone "$REPO_URL"
-cd devops-stack || exit
-git checkout "$TARGET_REVISION"
-cd - || exit
-
 echo Update app of apps without syncPolicy
-helm -n argocd upgrade app-of-apps devops-stack/argocd/app-of-apps \
+helm -n argocd upgrade app-of-apps argocd/app-of-apps \
 	-f "$APP_OF_APPS_VALUES_0" \
 	-f "$APP_OF_APPS_VALUES_1" \
 	-f "$APP_OF_APPS_VALUES_2" \
