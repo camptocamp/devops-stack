ed808dc99cc6d2d83b9d8fa804e92bf962308c84
Philippe <philippe.burgisser@camptocamp.com>
2021-04-26 12:13:01 +0200

    Adding OCP doc (#581)
    * Adding OCP doc
    
    * Improve doc
    
    * Adding example
    
    * Fixing syntax
    
    * Update docs/modules/ROOT/pages/howtos/quickstart_ocp_ipi_aws.adoc
    
    Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>
    
    * Update docs/modules/ROOT/pages/howtos/quickstart_ocp_ipi_aws.adoc
    
    Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>
    
    * Update docs/modules/ROOT/pages/howtos/quickstart_ocp_ipi_aws.adoc
    
    Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>
    
    * Update docs/modules/ROOT/pages/howtos/quickstart_ocp_ipi_aws.adoc
    
    Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>
    
    * Update docs/modules/ROOT/partials/ocp_ipi_instant_intro.adoc
    
    Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>
    
    * Update docs/modules/ROOT/partials/ocp_ipi_instant_intro.adoc
    
    Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>
    
    * Update docs/modules/ROOT/partials/ocp_kubeconfig.adoc
    
    Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>
    
    * Update docs/modules/ROOT/pages/howtos/quickstart_ocp_ipi_aws.adoc
    
    Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>
    
    * Update docs/modules/ROOT/partials/ocp_kubeconfig.adoc
    
    Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>
    
    Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>

---
+++ examples/ocp-ipi-aws/terraform/main.tf
@@ -0,0 +1,18 @@
+locals {
+  install_config_path = "install-config.yaml"
+  region              = "eu-west-1"
+  base_domain         = "example.com"
+  cluster_name        = "ocp"  
+}
+
+provider "aws" {
+  region = local.region
+}
+
+module "cluster" {
+  source              = "git::https://github.com/camptocamp/devops-stack.git//modules/openshift4/aws?ref=v0.32.0"
+  install_config_path = local.install_config_path
+  base_domain         = local.base_domain
+  cluster_name        = local.cluster_name
+  region              = local.region
+}
