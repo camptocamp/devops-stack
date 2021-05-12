8e076ee33dff0c09851dce81026ead7db88504f6
Mickaël Canévet <mickael.canevet@gmail.com>
2021-05-12 08:07:31 +0200

    Release 0.33.1

--- examples/eks-aws/terraform/main.tf
+++ examples/eks-aws/terraform/main.tf
@@ -23,7 +23,7 @@ data "aws_iam_role" "eks_admin" {
 }
 
 module "cluster" {
-  source = "git::https://github.com/camptocamp/devops-stack.git//modules/eks/aws?ref=v0.33.0"
+  source = "git::https://github.com/camptocamp/devops-stack.git//modules/eks/aws?ref=v0.33.1"
 
   cluster_name                         = "my-cluster"
   cluster_endpoint_public_access_cidrs = local.cluster_endpoint_public_access_cidrs
