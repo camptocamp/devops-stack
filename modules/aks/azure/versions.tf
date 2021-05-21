ce3f66da34082285c83a49dbc92e3575360b00f8
Mickaël Canévet <mickael.canevet@gmail.com>
2021-05-21 10:31:57 +0200

    feat(azuread): upgrade provider version 1.5.0

--- modules/aks/azure/versions.tf
+++ modules/aks/azure/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "1.0.0"
+      version = "1.5.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
