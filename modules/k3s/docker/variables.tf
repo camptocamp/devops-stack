72252aa8828609b8ba2482e113e1da6d36b27f13
Mickaël Canévet <mickael.canevet@gmail.com>
2021-05-21 10:03:33 +0200

    feat(k3s-docker): upgrade K3s to v1.20.7-k3s1

--- modules/k3s/docker/variables.tf
+++ modules/k3s/docker/variables.tf
@@ -1,7 +1,7 @@
 variable "k3s_version" {
   description = "The K3s version to use"
   type        = string
-  default     = "v1.20.5-k3s1"
+  default     = "v1.20.7-k3s1"
 }
 
 variable "server_ports" {
