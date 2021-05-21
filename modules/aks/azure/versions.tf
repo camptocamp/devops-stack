ac5125e0789b217fbf1ec2167b477b8b7a5897ef
Mickaël Canévet <mickael.canevet@gmail.com>
2021-05-21 11:02:29 +0200

    feat(kubernetes): upgrade kubernetes provider to 2.2.0

--- modules/aks/azure/versions.tf
+++ modules/aks/azure/versions.tf
@@ -10,7 +10,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.0.2"
+      version = "2.2.0"
     }
     helm = {
       source  = "hashicorp/helm"
