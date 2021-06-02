eee0c74453a94a5abc40e3c417abbfcf91072f3f
Julien Godin <julien.godin@camptocamp.com>
2021-06-02 12:07:55 +0200

    feat: output clusterid. This is used for group management

--- modules/aks/azure/outputs.tf
+++ modules/aks/azure/outputs.tf
@@ -2,6 +2,10 @@ output "node_resource_group" {
   value = module.cluster.node_resource_group
 }
 
+output "cluster_id" {
+  value = module.cluster.aks_id
+}
+
 output "prometheus_user_assigned_identity_principal_id" {
   value = azurerm_user_assigned_identity.kube_prometheus_stack_prometheus.principal_id
 }
