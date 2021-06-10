c25accc12f0ab0f740894b469a8041e04cd76997
Mickaël Canévet <mickael.canevet@gmail.com>
2021-06-10 10:48:29 +0200

    feat(aks): upgrade azurerm provider to 2.62.1

--- modules/aks/azure/versions.tf
+++ modules/aks/azure/versions.tf
@@ -6,7 +6,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.62.0"
+      version = "2.62.1"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
