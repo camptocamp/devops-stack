f05caec0370b55a088fa04efdd8fdf6a8abe6874
Mickaël Canévet <mickael.canevet@gmail.com>
2021-07-05 16:58:29 +0200

    feat(aks): add kube_admin_config output

--- modules/aks/azure/outputs.tf
+++ modules/aks/azure/outputs.tf
@@ -9,3 +9,14 @@ output "cluster_id" {
 output "prometheus_user_assigned_identity_principal_id" {
   value = azurerm_user_assigned_identity.kube_prometheus_stack_prometheus.principal_id
 }
+
+output "kube_admin_config" {
+  value = {
+    client_key             = module.cluster.admin_client_key
+    client_certificate     = module.cluster.admin_client_certificate
+    cluster_ca_certificate = module.cluster.admin_cluster_ca_certificate
+    host                   = module.cluster.admin_host
+    username               = module.cluster.admin_username
+    password               = module.cluster.admin_password
+  }
+}
