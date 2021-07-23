a9658863d2f0640cd977ec1c8d92a907c120a64f
Christopher Hornberger <christopher.hornberger@camptocamp.com>
2021-07-23 13:47:12 +0200

    WIP: test providers

--- tests/cce-opentelekomcloud/terraform/main.tf
+++ tests/cce-opentelekomcloud/terraform/main.tf
@@ -1,3 +1,11 @@
+#locals {
+#  ingress_listeners = [
+#    80,
+#    443,
+#  ]
+#}
+
+
 module "cluster" {
   source = "../../../modules/cce/opentelekomcloud"
 
@@ -37,3 +45,47 @@ module "cluster" {
     },
   }
 }
+
+#data "opentelekomcloud_cce_node_ids_v3" "node_ids" {
+#  cluster_id = var.cluster_id
+#}
+
+data "opentelekomcloud_cce_node_v3" "node_ip" {
+  cluster_id = var.cluster_id
+}
+
+
+
+#resource "opentelekomcloud_lb_loadbalancer_v2" "ingress" {
+#  name          = "devopsstack-ingress"
+#  vip_subnet_id = var.subnet_id
+#  #vip_address   = cidrhost(var.cidr, 3)
+#
+#  #security_group_ids = [
+#  #  data.opentelekomcloud_networking_secgroup_v2.FTTH_default.id,
+#  #  data.opentelekomcloud_networking_secgroup_v2.FTTH_custom_default.id,
+#}
+#
+#resource "opentelekomcloud_lb_listener_v2" "ingress" {
+#  count           = length(local.ingress_listeners)
+#  protocol        = "TCP"
+#  protocol_port   = local.ingress_listeners[count.index]
+#  loadbalancer_id = opentelekomcloud_lb_loadbalancer_v2.ingress.id
+#}
+#
+#resource "opentelekomcloud_lb_pool_v2" "ingress_haproxy" {
+#  count       = length(local.ingress_listeners)
+#  protocol    = "TCP"
+#  lb_method   = "ROUND_ROBIN"
+#  listener_id = opentelekomcloud_lb_listener_v2.ingress[count.index].id
+#}
+#
+#resource "opentelekomcloud_lb_member_v2" "ingress_haproxy" {
+#  count = 3 * length(local.ingress_listeners)
+#
+#  pool_id       = opentelekomcloud_lb_pool_v2.ingress_haproxy[abs(format("%0.f", count.index - 1 + 0.5) / 2)].id
+#  subnet_id     = var.subnet_id
+#  address       = module.haproxy.this_compute_instance_v2_access_ip_v4s[count.index % 2]
+#  protocol_port = local.ingress_listeners[abs(format("%0.f", count.index - 1 + 0.5) / 2)]
+#}
+#
--- tests/cce-opentelekomcloud/terraform/outputs.tf
+++ tests/cce-opentelekomcloud/terraform/outputs.tf
@@ -2,3 +2,11 @@ output "kubeconfig" {
   sensitive = true
   value     = module.cluster.kubeconfig
 }
+
+#output "node_ids" {
+#  value = data.opentelekomcloud_cce_node_ids_v3.node_ids.ids
+#}
+
+output "node_ips" {
+  value = data.opentelekomcloud_cce_node_v3.node_ips.*.public_ip
+}
