4580f2631b70da36a71b73c0acae8aa386fd6dcd
Mickaël Canévet <mickael.canevet@gmail.com>
2021-08-24 10:47:30 +0200

    feat(aks): set kubernetes version to current default

--- modules/aks/azure/variables.tf
+++ modules/aks/azure/variables.tf
@@ -6,7 +6,7 @@ variable "base_domain" {
 variable "kubernetes_version" {
   description = "Specify which Kubernetes release to use."
   type        = string
-  default     = "1.18.14"
+  default     = "1.20.7"
 }
 
 variable "resource_group_name" {
