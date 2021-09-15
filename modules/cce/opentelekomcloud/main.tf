ead90c7502e23fb1b23fcf20478cd07c225cd943
Mickaël Canévet <mickael.canevet@gmail.com>
2021-09-15 09:33:00 +0200

    feat(keycloak): rename client to devops-stack-applications

--- modules/cce/opentelekomcloud/main.tf
+++ modules/cce/opentelekomcloud/main.tf
@@ -53,7 +53,7 @@ module "argocd" {
     oauth_url     = format("https://keycloak.apps.%s.%s/auth/realms/kubernetes/protocol/openid-connect/auth", var.cluster_name, local.base_domain)
     token_url     = format("https://keycloak.apps.%s.%s/auth/realms/kubernetes/protocol/openid-connect/token", var.cluster_name, local.base_domain)
     api_url       = format("https://keycloak.apps.%s.%s/auth/realms/kubernetes/protocol/openid-connect/userinfo", var.cluster_name, local.base_domain)
-    client_id     = "applications"
+    client_id     = "devops-stack-applications"
     client_secret = random_password.clientsecret.result
     oauth2_proxy_extra_args = [
       "--insecure-oidc-skip-issuer-verification=true",
