17582fd7b454c425ebdf9e6e80c5e34c30d60f54
Raphaël Pinson <raphael.pinson@camptocamp.com>
2022-01-10 13:26:44 -0500

    move cert-manager k3s conf to module
    Signed-off-by: Raphaël Pinson <raphael.pinson@camptocamp.com>

--- tests/k3s-docker/main.tf
+++ tests/k3s-docker/main.tf
@@ -72,7 +72,7 @@ module "monitoring" {
 
 
 module "cert-manager" {
-  source = "git::https://github.com/camptocamp/devops-stack-module-cert-manager.git//terraform"
+  source = "git::https://github.com/camptocamp/devops-stack-module-cert-manager.git//terraform/k3s"
 
   cluster_name   = var.cluster_name
   oidc           = module.cluster.oidc
@@ -89,23 +89,23 @@ module "cert-manager" {
   depends_on = [ module.monitoring ]
 }
 
-module "argocd" {
-  source = "git::https://github.com/camptocamp/devops-stack-module-argocd.git//terraform"
-
-  cluster_name   = var.cluster_name
-  oidc           = module.cluster.oidc
-  argocd         = {
-    namespace = module.cluster.argocd_namespace
-    server_secretkey = module.cluster.argocd_server_secretkey
-    accounts_pipeline_tokens = module.cluster.argocd_accounts_pipeline_tokens
-    server_admin_password = module.cluster.argocd_server_admin_password
-    domain = module.cluster.argocd_domain
-  }
-  base_domain    = module.cluster.base_domain
-  cluster_issuer = "ca-issuer"
-
-  depends_on = [ module.cert-manager ]
-}
+#module "argocd" {
+#  source = "git::https://github.com/camptocamp/devops-stack-module-argocd.git//terraform"
+#
+#  cluster_name   = var.cluster_name
+#  oidc           = module.cluster.oidc
+#  argocd         = {
+#    namespace = module.cluster.argocd_namespace
+#    server_secretkey = module.cluster.argocd_server_secretkey
+#    accounts_pipeline_tokens = module.cluster.argocd_accounts_pipeline_tokens
+#    server_admin_password = module.cluster.argocd_server_admin_password
+#    domain = module.cluster.argocd_domain
+#  }
+#  base_domain    = module.cluster.base_domain
+#  cluster_issuer = "ca-issuer"
+#
+#  depends_on = [ module.cert-manager ]
+#}
 
 #module "myownapp" {
 #  source = "git::https://github.com/camptocamp/devops-stack-module-applicationset.git"
