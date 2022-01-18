30cd6f603c1fde8fe1a6df61aa93ce540a71b763
Raphaël Pinson <raphael.pinson@camptocamp.com>
2022-01-18 13:14:37 -0500

    feat: invert oidc logic for k3s
    Signed-off-by: Raphaël Pinson <raphael.pinson@camptocamp.com>

--- modules/k3s/outputs.tf
+++ modules/k3s/outputs.tf
@@ -2,10 +2,6 @@ output "base_domain" {
   value = local.base_domain
 }
 
-output "oidc" {
-  value = local.oidc
-}
-
 output "kubernetes" {
   value = {
     host                   = local.kubernetes_host
@@ -14,14 +10,3 @@ output "kubernetes" {
     cluster_ca_certificate = local.kubernetes_cluster_ca_certificate
   }
 }
-
-output "keycloak_users" {
-  value     = { for username, infos in local.keycloak_user_map : username => lookup(infos, "password") }
-  sensitive = true
-}
-
-#output "keycloak_admin_password" {
-#  description = "The password of Keycloak's admin user."
-#  value       = data.kubernetes_secret.keycloak_admin_password.data.ADMIN_PASSWORD
-#  sensitive   = true
-#}
