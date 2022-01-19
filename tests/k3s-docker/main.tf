097a45a87187796e6403b5df9e67e21803d18dbb
Raphaël Pinson <raphael.pinson@camptocamp.com>
2022-01-19 11:58:40 -0500

    fix: adapt usage of traefik module
    Signed-off-by: Raphaël Pinson <raphael.pinson@camptocamp.com>

--- tests/k3s-docker/main.tf
+++ tests/k3s-docker/main.tf
@@ -27,13 +27,9 @@ provider "argocd" {
 module "ingress" {
   source = "git::https://github.com/camptocamp/devops-stack-module-traefik.git//modules"
 
-  cluster_name   = var.cluster_name
-  argocd         = {
-    namespace = module.cluster.argocd_namespace
-  }
-  base_domain    = module.cluster.base_domain
-
-  #profiles = [ "default", "eks" ]
+  cluster_name     = var.cluster_name
+  argocd_namespace = module.cluster.argocd_namespace
+  base_domain      = module.cluster.base_domain
 }
 
 module "oidc" {
