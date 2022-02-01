6dba665ce17b067876710eb04e7f4bb056e5f2c9
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2022-02-01 11:27:05 +0100

    feat: start refactoring SKS module to argo_modules

--- tests/sks-exoscale/main.tf
+++ tests/sks-exoscale/main.tf
@@ -4,6 +4,27 @@ module "cluster" {
   cluster_name = var.cluster_name
   zone         = "de-fra-1"
 
-  repo_url        = var.repo_url
-  target_revision = var.target_revision
+  kubernetes_version = "1.22.5"
+}
+
+provider "argocd" {
+  server_addr = "127.0.0.1:8080"
+  auth_token  = module.cluster.argocd_auth_token
+  insecure = true
+  plain_text = true
+  port_forward = true
+  port_forward_with_namespace = module.cluster.argocd_namespace
+
+  kubernetes {
+    host                   = module.cluster.kubernetes.host
+    client_certificate     = module.cluster.kubernetes.client_certificate
+    client_key             = module.cluster.kubernetes.client_key
+    cluster_ca_certificate = module.cluster.kubernetes.cluster_ca_certificate
+  }
+}
+
+module "ingress" {
+  source = "git::https://github.com/camptocamp/devops-stack-module-traefik.git//modules/sks"
+
+  cluster_info = module.cluster.info
 }
