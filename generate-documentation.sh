e068493076fc5ee75b0aeddfed77754066396684
Yann Soubeyrand <yann.soubeyrand@camptocamp.com>
2022-02-11 10:43:00 +0100

    docs: upgrade Antora

---
+++ generate-documentation.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+cd "$(dirname $0)"
+
+mkdir -p 'supplemental_ui'
+
+cp -RT "$(yarn global dir)/node_modules/@antora/lunr-extension/supplemental_ui/" 'supplemental_ui'
+
+antora generate 'antora-playbook.yml'
