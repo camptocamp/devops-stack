e631f5bf753625905cd09e8425df2e103f57b565
Federico Sismondi <federico.sismondi@camptocamp.com>
2022-02-16 11:19:38 +0100

    docs: generate doc for new release

--- modules/variables.tf
+++ modules/variables.tf
@@ -12,7 +12,7 @@ variable "repo_url" {
 variable "target_revision" {
   description = "The source target revision of ArgoCD's app of apps."
   type        = string
-  default     = "v0.53.0"
+  default     = "v0.54.0"
 }
 
 variable "app_of_apps_values_overrides" {
