9fa840d80eeba0ec07c6b415dbe62f269d5464c4
Yann Soubeyrand <yann.soubeyrand@camptocamp.com>
2022-02-18 20:15:36 +0100

    ci: refactor workflows to decouple website build from releases

--- generate-documentation.sh
+++ generate-documentation.sh
@@ -6,4 +6,4 @@ mkdir -p 'supplemental_ui'
 
 cp -RT "$(yarn global dir)/node_modules/@antora/lunr-extension/supplemental_ui/" 'supplemental_ui'
 
-antora generate 'antora-playbook.yml'
+antora generate 'antora-playbook.yml' --to-dir 'public/docs'
