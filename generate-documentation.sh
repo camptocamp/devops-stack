c13cf57d88f9d75248da481d7bb85ba85d74a7ee
Yann Soubeyrand <yann.soubeyrand@camptocamp.com>
2022-02-21 10:08:34 +0100

    docs: move supplemental UI files to documentation directory

--- generate-documentation.sh
+++ generate-documentation.sh
@@ -2,8 +2,6 @@
 
 cd "$(dirname $0)"
 
-mkdir -p 'supplemental_ui'
-
-cp -RT "$(yarn global dir)/node_modules/@antora/lunr-extension/supplemental_ui/" 'supplemental_ui'
+cp -RT "$(yarn global dir)/node_modules/@antora/lunr-extension/supplemental_ui/" 'docs/supplemental_ui'
 
 antora generate 'antora-playbook.yml' --to-dir 'public/docs'
