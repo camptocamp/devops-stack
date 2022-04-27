474c70871c93f01af44f9dd9a31a94205903d778
Federico Sismondi <federico.sismondi@camptocamp.com>
2022-04-27 17:57:38 +0200

    chore: tf fmt

--- modules/aks/azure/main.tf
+++ modules/aks/azure/main.tf
@@ -8,6 +8,7 @@ locals {
   kubernetes_cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_admin_config.0.cluster_ca_certificate)
 
   kubeconfig           = data.azurerm_kubernetes_cluster.cluster.kube_admin_config_raw
+
 }
 
 data "azurerm_resource_group" "this" {
