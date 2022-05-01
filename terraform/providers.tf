73df1200a5170335688945ec4bcde82840136477
Nora Kennedy <nora.dbu@gmail.com>
2022-05-01 09:22:58 -0500

    bunch of breaking changes

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -54,3 +54,7 @@ provider "kubernetes" {
   client_key             = local.kubernetes_client_key
   cluster_ca_certificate = local.kubernetes_cluster_ca_certificate
 }
+
+provider "azurerm" {
+  features {}
+}
