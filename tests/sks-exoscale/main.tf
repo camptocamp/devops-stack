0f960f4545b9eb7dc80e2e381e6ec3294bbaf97b
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2022-07-15 17:57:51 +0200

    feat(sks tests): adapt for changes in sks module and others

--- tests/sks-exoscale/main.tf
+++ tests/sks-exoscale/main.tf
@@ -1,81 +1,137 @@
-module "cluster" {
+module "sks" {
   source = "../../modules/sks/exoscale"
 
-  cluster_name = var.cluster_name
+  cluster_name = "ckg-test"
   zone         = var.zone
 
-  kubernetes_version = "1.22.5"
+  kubernetes_version = "1.23.8"
+  #kubernetes_version = "1.24.2"
 
   nodepools = {
-    "router-${var.cluster_name}" = {
+    "router-${module.sks.cluster_name}" = {
       size          = 2
       instance_type = "standard.large"
     }
 
-    "compute-${var.cluster_name}" = {
+    "compute-${module.sks.cluster_name}" = {
       size          = 2
       instance_type = "standard.large"
     }
   }
+}
+
+provider "kubernetes" {
+  host                   = module.sks.kubernetes_host
+  client_key             = module.sks.kubernetes_client_key
+  client_certificate     = module.sks.kubernetes_client_certificate
+  cluster_ca_certificate = module.sks.kubernetes_cluster_ca_certificate
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.sks.kubernetes_host
+    client_key             = module.sks.kubernetes_client_key
+    client_certificate     = module.sks.kubernetes_client_certificate
+    cluster_ca_certificate = module.sks.kubernetes_cluster_ca_certificate
+  }
+}
+
+locals {
+  argocd_namespace = "argocd"
+  argocd_oidc = {
+    name         = "OIDC"
+    issuer       = module.oidc.oidc.issuer_url
+    clientID     = module.oidc.oidc.client_id
+    clientSecret = module.oidc.oidc.client_secret
+    requestedIDTokenClaims = {
+      groups = {
+        essential = true
+      }
+    }
+    requestedScopes = [
+      "openid", "profile", "email"
+    ]
+  }
+}
+
+module "argocd_bootstrap" {
+  source = "git::https://github.com/camptocamp/devops-stack-module-argocd.git//bootstrap"
+
+  cluster_name     = module.sks.cluster_name
+  base_domain      = module.sks.base_domain
+  cluster_issuer   = "letsencrypt-prod"
+
+  depends_on = [module.sks]
 }
 
 provider "argocd" {
   server_addr = "127.0.0.1:8080"
-  auth_token  = module.cluster.argocd_auth_token
+  auth_token = module.argocd_bootstrap.argocd_auth_token
   insecure = true
   plain_text = true
   port_forward = true
-  port_forward_with_namespace = module.cluster.argocd_namespace
+  port_forward_with_namespace = local.argocd_namespace
 
   kubernetes {
-    host                   = module.cluster.kubernetes.host
-    client_certificate     = module.cluster.kubernetes.client_certificate
-    client_key             = module.cluster.kubernetes.client_key
-    cluster_ca_certificate = module.cluster.kubernetes.cluster_ca_certificate
+    host                   = module.sks.kubernetes_host
+    client_key             = module.sks.kubernetes_client_key
+    client_certificate     = module.sks.kubernetes_client_certificate
+    cluster_ca_certificate = module.sks.kubernetes_cluster_ca_certificate
   }
 }
 
 module "ingress" {
-  source = "git::https://github.com/camptocamp/devops-stack-module-traefik.git//modules/sks"
+  source = "git::https://github.com/camptocamp/devops-stack-module-traefik.git//sks"
 
-  cluster_name     = var.cluster_name
-  argocd_namespace = module.cluster.argocd_namespace
-  base_domain      = module.cluster.base_domain
+  cluster_name     = module.sks.cluster_name
+  argocd_namespace = local.argocd_namespace
+  base_domain      = module.sks.base_domain
+
+  depends_on = [module.argocd_bootstrap]
 }
 
 module "oidc" {
-  source = "git::https://github.com/camptocamp/devops-stack-module-keycloak.git//modules"
+  source = "git::https://github.com/camptocamp/devops-stack-module-keycloak.git/"
 
-  cluster_name   = var.cluster_name
+  cluster_name   = module.sks.cluster_name
   argocd         = {
-    namespace = module.cluster.argocd_namespace
-    domain    = module.cluster.argocd_domain
+    namespace = local.argocd_namespace
+    domain    = module.argocd_bootstrap.argocd_domain
   }
-  base_domain    = module.cluster.base_domain
+  base_domain    = module.sks.base_domain
   cluster_issuer = "ca-issuer"
 
-  depends_on = [ module.ingress ]
+  depends_on = [ module.sks, module.argocd_bootstrap ]
 }
 
 module "monitoring" {
-  source = "git::https://github.com/camptocamp/devops-stack-module-kube-prometheus-stack.git//modules"
+  source = "git::https://github.com/camptocamp/devops-stack-module-kube-prometheus-stack.git/"
 
-  cluster_name     = var.cluster_name
-  oidc             = module.oidc.oidc
-  argocd_namespace = module.cluster.argocd_namespace
-  base_domain    = module.cluster.base_domain
-  cluster_issuer = "ca-issuer"
+  cluster_name     = module.sks.cluster_name
+  argocd_namespace = local.argocd_namespace
+  base_domain      = module.sks.base_domain
+  cluster_issuer   = "ca-issuer"
   metrics_archives = {}
 
-  depends_on = [ module.oidc ]
+  prometheus = {
+    oidc = module.oidc.oidc
+  }
+  alertmanager = {
+    oidc = module.oidc.oidc
+  }
+  grafana = {
+    oidc = module.oidc.oidc
+  }
+
+  depends_on = [ module.argocd_bootstrap ]
 }
 
 module "loki-stack" {
-  source = "git::https://github.com/camptocamp/devops-stack-module-loki-stack.git//modules"
+  source = "git::https://github.com/camptocamp/devops-stack-module-loki-stack.git/"
 
-  cluster_name     = var.cluster_name
-  argocd_namespace = module.cluster.argocd_namespace
-  base_domain      = module.cluster.base_domain
+  cluster_name     = module.sks.cluster_name
+  argocd_namespace = local.argocd_namespace
+  base_domain      = module.sks.base_domain
 
   #  minio = {
   #    access_key = module.storage.access_key
@@ -87,38 +143,39 @@ module "loki-stack" {
 
 
 module "cert-manager" {
-  source = "git::https://github.com/camptocamp/devops-stack-module-cert-manager.git//modules/self-signed"
+  source = "git::https://github.com/camptocamp/devops-stack-module-cert-manager.git//self-signed"
 
-  cluster_name     = var.cluster_name
-  argocd_namespace = module.cluster.argocd_namespace
-  base_domain      = module.cluster.base_domain
+  cluster_name     = module.sks.cluster_name
+  argocd_namespace = local.argocd_namespace
+  base_domain      = module.sks.base_domain
 
   depends_on = [ module.monitoring ]
 }
 
 module "argocd" {
-  source = "git::https://github.com/camptocamp/devops-stack-module-argocd.git//modules"
+  source = "git::https://github.com/camptocamp/devops-stack-module-argocd.git/"
 
-  cluster_name   = var.cluster_name
-  oidc           = module.oidc.oidc
+  cluster_name   = module.sks.cluster_name
+  oidc           = local.argocd_oidc
   argocd         = {
-    namespace = module.cluster.argocd_namespace
-    server_secretkey = module.cluster.argocd_server_secretkey
-    accounts_pipeline_tokens = module.cluster.argocd_accounts_pipeline_tokens
-    server_admin_password = module.cluster.argocd_server_admin_password
-    domain = module.cluster.argocd_domain
-    admin_enabled = true
+    namespace                = local.argocd_namespace
+    server_secretkey         = module.argocd_bootstrap.argocd_server_secretkey
+    accounts_pipeline_tokens = module.argocd_bootstrap.argocd_accounts_pipeline_tokens
+    server_admin_password    = module.argocd_bootstrap.argocd_server_admin_password
+    domain                   = module.argocd_bootstrap.argocd_domain
+    admin_enabled            = true
   }
-  base_domain    = module.cluster.base_domain
+  base_domain    = module.sks.base_domain
   cluster_issuer = "ca-issuer"
+  bootstrap_values = module.argocd_bootstrap.bootstrap_values
 
   depends_on = [ module.cert-manager, module.monitoring ]
 }
 
 module "my-apps" {
-  source = "git::https://github.com/camptocamp/devops-stack-module-applicationset.git//modules"
+  source = "git::https://github.com/camptocamp/devops-stack-module-applicationset.git/"
 
-  argocd_namespace = module.cluster.argocd_namespace
+  argocd_namespace = local.argocd_namespace
 
   name = "my-apps"
   namespace = "my-apps"
--- tests/sks-exoscale/outputs.tf
+++ tests/sks-exoscale/outputs.tf
@@ -1,46 +1,46 @@
-output "base_domain" {
-  value = module.cluster.base_domain
-}
-
-output "argocd_auth_token" {
-  sensitive = true
-  value     = module.cluster.argocd_auth_token
-}
-
-output "argocd_server_admin_password" {
-  sensitive = true
-  value     = module.cluster.argocd_server_admin_password
-}
-
-output "kubeconfig" {
-  sensitive = true
-  value     = module.cluster.kubeconfig
-}
-
-output "argocd_server" {
-  value = module.cluster.argocd_server
-}
-
-output "repo_url" {
-  value = module.cluster.repo_url
-}
-
-output "target_revision" {
-  value = module.cluster.target_revision
-}
-
+#output "base_domain" {
+#  value = module.sks.base_domain
+#}
+#
+#output "argocd_auth_token" {
+#  sensitive = true
+#  value     = module.sks.argocd_auth_token
+#}
+#
+#output "argocd_server_admin_password" {
+#  sensitive = true
+#  value     = module.sks.argocd_server_admin_password
+#}
+#
+#output "kubeconfig" {
+#  sensitive = true
+#  value     = module.sks.kubeconfig
+#}
+#
+#output "argocd_server" {
+#  value = module.sks.argocd_server
+#}
+#
+#output "repo_url" {
+#  value = module.sks.repo_url
+#}
+#
+#output "target_revision" {
+#  value = module.sks.target_revision
+#}
+#
 #output "argocd_url" {
-#  value = format("https://argocd.apps.%s.%s", var.cluster_name, module.cluster.base_domain)
+#  value = format("https://argocd.apps.%s.%s", var.cluster_name, module.sks.base_domain)
 #}
 #
 #output "grafana_url" {
-#  value = format("https://grafana.apps.%s.%s", var.cluster_name, module.cluster.base_domain)
+#  value = format("https://grafana.apps.%s.%s", var.cluster_name, module.sks.base_domain)
 #}
 #
 #output "prometheus_url" {
-#  value = format("https://prometheus.apps.%s.%s", var.cluster_name, module.cluster.base_domain)
+#  value = format("https://prometheus.apps.%s.%s", var.cluster_name, module.sks.base_domain)
 #}
 #
 #output "alertmanager_url" {
-#  value = format("https://alertmanager.apps.%s.%s", var.cluster_name, module.cluster.base_domain)
+#  value = format("https://alertmanager.apps.%s.%s", var.cluster_name, module.sks.base_domain)
 #}
