8cebd86787e1d8d6783e7ef34e307d537f58f0e7
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2022-07-22 11:55:05 +0200

    chore(sks): put nlb config in separate file

--- modules/sks/exoscale/main.tf
+++ modules/sks/exoscale/main.tf
@@ -13,48 +13,6 @@ module "cluster" {
   nodepools = var.nodepools
 }
 
-resource "exoscale_nlb" "this" {
-  zone = var.zone
-  name = format("ingresses-%s", var.cluster_name)
-}
-
-resource "exoscale_nlb_service" "http" {
-  zone             = exoscale_nlb.this.zone
-  name             = "ingress-contoller-http"
-  nlb_id           = exoscale_nlb.this.id
-  instance_pool_id = module.cluster.nodepools[local.router_nodepool].instance_pool_id
-  protocol         = "tcp"
-  port             = 80
-  target_port      = 80
-  strategy         = "round-robin"
-
-  healthcheck {
-    mode     = "tcp"
-    port     = 80
-    interval = 5
-    timeout  = 3
-    retries  = 1
-  }
-}
-
-resource "exoscale_nlb_service" "https" {
-  zone             = exoscale_nlb.this.zone
-  name             = "ingress-contoller-https"
-  nlb_id           = exoscale_nlb.this.id
-  instance_pool_id = module.cluster.nodepools[local.router_nodepool].instance_pool_id
-  protocol         = "tcp"
-  port             = 443
-  target_port      = 443
-  strategy         = "round-robin"
-
-  healthcheck {
-    mode     = "tcp"
-    port     = 443
-    interval = 5
-    timeout  = 3
-    retries  = 1
-  }
-}
 
 resource "exoscale_security_group_rule" "http" {
   security_group_id = module.cluster.this_security_group_id
