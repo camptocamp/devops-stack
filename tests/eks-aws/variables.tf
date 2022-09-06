00233233ddb9ab73fa614f5864e0549fad8544ae
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2022-09-06 14:58:20 +0200

    refactor: terraform fmt

--- tests/eks-aws/variables.tf
+++ tests/eks-aws/variables.tf
@@ -1,5 +1,5 @@
 locals {
-#  repo_url = "https://github.com/camptocamp/devops-stack.git"
-#  target_revision = "master"
-#  cluster_name = "devops-stack-v1-dev"
+  #  repo_url = "https://github.com/camptocamp/devops-stack.git"
+  #  target_revision = "master"
+  #  cluster_name = "devops-stack-v1-dev"
 }
