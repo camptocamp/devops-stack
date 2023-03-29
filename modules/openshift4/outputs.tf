e3028ab2f61dcc07a2edfe39d14fce5f86f41fa9
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-03-29 12:14:19 +0200

    chore: remove legacy modules no longer in use

--- modules/openshift4/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "console_url" {
-  value = "https://console-openshift-console.apps.${var.cluster_name}.${var.base_domain}"
-}
-
-output "kubeadmin_password" {
-  value     = module.cluster.kubeadmin_password
-  sensitive = true
-}
