99c9df1fafe119033716e902181503d03ac8e28a
Gonçalo Heleno <33546359+lentidas@users.noreply.github.com>
2023-04-03 15:41:57 +0200

    chore: update and cleanup v1 branch for v1 release (#975)
    * ci: add ci changes from master branch
    
    * chore: remove old argocd templates used in the monolithic version
    
    * chore: remove old deployment examples
    
    I think these examples were used to show a deployment of the DevOps Stack in different platforms. Most of the code seems to be deprecated and I find theses examples redundant with what we have been putting in the tests folder. In my view, the tests folder should become the new examples folder.
    
    * chore: remove AKS cluster module as it is no longer used
    
    * chore: remove EKS module since it is now on its own repository
    
    https://github.com/camptocamp/devops-stack-module-cluster-eks
    
    * chore: remove KinD module since it was revamped to another repository
    
    https://github.com/camptocamp/devops-stack-module-cluster-kind
    
    * chore: remove legacy modules no longer in use
    
    * chore: remove legacy tests no longer in use
    
    * chore: rename folder from site to website
    
    * chore: add changes to the website from master
    
    * chore: update submodules file
    
    * chore: add miscellaneous files from the master branch
    
    * chore: convert security notice to .adoc
    
    * chore: remove legacy scripts and workflows
    
    * feat: remove legacy changelog
    
    * docs: add PR template
    
    * ci: add small improvements to the workflows of the modules
    
    * ci: homogenize workflow names
    
    * ci: revamp documentation workflow for the v1
    
    * ci: refactor code and bump version
    
    * ci: rename workflows for clarity
    
    * refactor: rename tests folder to examples
    
    I think this folder is more appropriately named as examples.
    
    * ci: simplify terraform-docs workflows with variables
    
    * fix: rename branches on deployment condition
    
    * fix(website): change links
    
    * ci: refactor some small things on the pipelines
    
    * ci: deactivate project ci temporarly

--- argocd/README.tmpl.md
+++
@@ -1,9 +0,0 @@
----
-title: {{ template "chart.name" . }}
----
-
-{{ template "chart.description" . }}
-
-{{ template "chart.requirementsSection" . }}
-
-{{ template "chart.valuesSection" . }}
--- tests/cce-opentelekomcloud/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "kubeconfig" {
-  sensitive = true
-  value     = module.cluster.kubeconfig
-}
-
-output "keycloak_users" {
-  sensitive = true
-  value     = module.cluster.keycloak_users
-}
