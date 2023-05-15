333e967caa17580d67400f5d7cffa1aecf391322
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-05-15 15:53:59 +0200

    chore: remove unused outputs

--- examples/kind/outputs.tf
+++ examples/kind/outputs.tf
@@ -1,10 +1,3 @@
-# TODO: is this needed ?
-# output "argocd_auth_token" {
-#   description = "" # TODO
-#   value     = module.argocd_bootstrap.argocd_auth_token
-#   sensitive = true
-# }
-
 output "kubernetes_kubeconfig" {
   description = "Configuration that can be copied into `.kube/config in order to access the cluster with `kubectl`."
   value       = module.kind.raw_kubeconfig
@@ -20,11 +13,5 @@ output "keycloak_admin_credentials" {
 output "keycloak_users" {
   description = "Map containing the credentials of each created user."
   value       = module.oidc.devops_stack_users_passwords
-  sensitive   = true
-}
-
-output "minio_root_user_credentials" {
-  description = "Credentials for the administrator user of the MinIO server."
-  value       = module.minio.minio_root_user_credentials
   sensitive   = true
 }
