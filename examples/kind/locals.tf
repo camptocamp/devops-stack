03db242ac9e6a1c4973aa3515445dc3638b83229
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-05-17 14:06:45 +0200

    feat: update KinD example with newer module versions

--- examples/kind/locals.tf
+++ examples/kind/locals.tf
@@ -1,6 +1,7 @@
 locals {
-  kubernetes_version = "v1.27.1"
-  cluster_name       = "kind-cluster"
-  base_domain        = format("%s.nip.io", replace(module.traefik.external_ip, ".", "-"))
-  cluster_issuer     = "ca-issuer"
+  kubernetes_version     = "v1.27.1"
+  cluster_name           = "kind-cluster"
+  base_domain            = format("%s.nip.io", replace(module.traefik.external_ip, ".", "-"))
+  cluster_issuer         = "ca-issuer"
+  enable_service_monitor = false
 }
