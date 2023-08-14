ef492396f44b35e71cb7e3c74d3fb9ec8135602b
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2023-08-14 11:25:37 +0200

    feat(examples/eks): split helloworld apps into separate apps.tf file

---
+++ examples/eks/apps.tf
@@ -0,0 +1,74 @@
+module "helloworld_apps" {
+  source = "git::https://github.com/camptocamp/devops-stack-module-applicationset.git?ref=v2.0.1"
+
+  depends_on = [module.argocd]
+
+  name                   = "helloworld-apps"
+  argocd_namespace       = local.argocd_namespace
+  project_dest_namespace = "*"
+  project_source_repo    = "https://github.com/camptocamp/devops-stack-helloworld-templates.git"
+
+  generators = [
+    {
+      git = {
+        repoURL  = "https://github.com/camptocamp/devops-stack-helloworld-templates.git"
+        revision = "main"
+
+        directories = [
+          {
+            path = "apps/*"
+          }
+        ]
+      }
+    }
+  ]
+  template = {
+    metadata = {
+      name = "{{path.basename}}"
+    }
+
+    spec = {
+      project = "helloworld-apps"
+
+      source = {
+        repoURL        = "https://github.com/camptocamp/devops-stack-helloworld-templates.git"
+        targetRevision = "main"
+        path           = "{{path}}"
+
+        helm = {
+          valueFiles = []
+          # The following value defines this global variables that will be available to all apps in apps/*
+          # These are needed to generate the ingresses containing the name and base domain of the cluster.
+          values = <<-EOT
+            cluster:
+              name: "${module.eks.cluster_name}"
+              domain: "${module.eks.base_domain}"
+            apps:
+              traefik_dashboard: false # TODO Add variable when we configure the Traefik Dashboard
+              grafana: true
+              prometheus: true
+              thanos: true
+              alertmanager: true
+          EOT
+        }
+      }
+
+      destination = {
+        name      = "in-cluster"
+        namespace = "{{path.basename}}"
+      }
+
+      syncPolicy = {
+        automated = {
+          allowEmpty = false
+          selfHeal   = true
+          prune      = true
+        }
+        syncOptions = [
+          "CreateNamespace=true"
+        ]
+      }
+    }
+  }
+}
+
