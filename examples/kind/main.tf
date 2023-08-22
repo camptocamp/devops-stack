2a6d4f5ebe88e8f087af37f9ade209594eed6163
modridi <mohamed-amine.dridi@camptocamp.com>
2023-08-22 16:24:16 +0200

    update

--- examples/kind/main.tf
+++ examples/kind/main.tf
@@ -250,6 +250,7 @@ module "thanos" {
 
 module "kube-prometheus-stack" {
   source          = "git::https://github.com/camptocamp/devops-stack-module-kube-prometheus-stack//kind?ref=ISDEVOPS-233"
+  # source = "../../../devops-stack-modules/devops-stack-module-kube-prometheus-stack/kind"
   target_revision = "ISDEVOPS-233"
 
   cluster_name     = local.cluster_name
@@ -282,53 +283,53 @@ module "kube-prometheus-stack" {
   }
 }
 
-module "argocd" {
-  source = "git::https://github.com/camptocamp/devops-stack-module-argocd.git?ref=v2.0.0"
+# module "argocd" {
+#   source = "git::https://github.com/camptocamp/devops-stack-module-argocd.git?ref=v2.0.0"
 
-  base_domain              = local.base_domain
-  cluster_name             = local.cluster_name
-  cluster_issuer           = local.cluster_issuer
-  server_secretkey         = module.argocd_bootstrap.argocd_server_secretkey
-  accounts_pipeline_tokens = module.argocd_bootstrap.argocd_accounts_pipeline_tokens
+#   base_domain              = local.base_domain
+#   cluster_name             = local.cluster_name
+#   cluster_issuer           = local.cluster_issuer
+#   server_secretkey         = module.argocd_bootstrap.argocd_server_secretkey
+#   accounts_pipeline_tokens = module.argocd_bootstrap.argocd_accounts_pipeline_tokens
 
-  oidc = {
-    name         = "OIDC"
-    issuer       = module.oidc.oidc.issuer_url
-    clientID     = module.oidc.oidc.client_id
-    clientSecret = module.oidc.oidc.client_secret
-    requestedIDTokenClaims = {
-      groups = {
-        essential = true
-      }
-    }
-  }
+#   oidc = {
+#     name         = "OIDC"
+#     issuer       = module.oidc.oidc.issuer_url
+#     clientID     = module.oidc.oidc.client_id
+#     clientSecret = module.oidc.oidc.client_secret
+#     requestedIDTokenClaims = {
+#       groups = {
+#         essential = true
+#       }
+#     }
+#   }
 
-  dependency_ids = {
-    traefik               = module.traefik.id
-    cert-manager          = module.cert-manager.id
-    oidc                  = module.oidc.id
-    kube-prometheus-stack = module.kube-prometheus-stack.id
-  }
-}
+#   dependency_ids = {
+#     traefik               = module.traefik.id
+#     cert-manager          = module.cert-manager.id
+#     oidc                  = module.oidc.id
+#     kube-prometheus-stack = module.kube-prometheus-stack.id
+#   }
+# }
 
-module "metrics_server" {
-  source = "git::https://github.com/camptocamp/devops-stack-module-application.git?ref=v1.2.2"
+# # module "metrics_server" {
+# #   source = "git::https://github.com/camptocamp/devops-stack-module-application.git?ref=v1.2.2"
 
-  name             = "metrics-server"
-  argocd_namespace = module.argocd_bootstrap.argocd_namespace
+# #   name             = "metrics-server"
+# #   argocd_namespace = module.argocd_bootstrap.argocd_namespace
 
-  source_repo            = "https://github.com/kubernetes-sigs/metrics-server.git"
-  source_repo_path       = "charts/metrics-server"
-  source_target_revision = "metrics-server-helm-chart-3.8.3"
-  destination_namespace  = "kube-system"
+# #   source_repo            = "https://github.com/kubernetes-sigs/metrics-server.git"
+# #   source_repo_path       = "charts/metrics-server"
+# #   source_target_revision = "metrics-server-helm-chart-3.8.3"
+# #   destination_namespace  = "kube-system"
 
-  helm_values = [{
-    args = [
-      "--kubelet-insecure-tls" # Ignore self-signed certificates of the KinD cluster
-    ]
-  }]
+# #   helm_values = [{
+# #     args = [
+# #       "--kubelet-insecure-tls" # Ignore self-signed certificates of the KinD cluster
+# #     ]
+# #   }]
 
-  dependency_ids = {
-    argocd = module.argocd.id
-  }
-}
+# #   dependency_ids = {
+# #     argocd = module.argocd.id
+# #   }
+# # }
