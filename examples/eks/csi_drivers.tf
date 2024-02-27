25c1a9dae5d9632a3bb660918a72bd06a49921d8
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2024-02-27 10:59:27 +0100

    docs(eks): update example with new module versions

--- examples/eks/csi_drivers.tf
+++ examples/eks/csi_drivers.tf
@@ -35,7 +35,7 @@ resource "aws_efs_mount_target" "eks" {
 }
 
 module "efs" {
-  source = "git::https://github.com/camptocamp/devops-stack-module-efs-csi-driver.git?ref=v3.0.0"
+  source = "git::https://github.com/camptocamp/devops-stack-module-efs-csi-driver.git?ref=v3.1.0"
 
   cluster_name   = local.cluster_name
   argocd_project = module.eks.cluster_name
@@ -52,7 +52,7 @@ module "efs" {
 }
 
 module "ebs" {
-  source = "git::https://github.com/camptocamp/devops-stack-module-ebs-csi-driver.git?ref=v3.0.0"
+  source = "git::https://github.com/camptocamp/devops-stack-module-ebs-csi-driver.git?ref=v3.1.0"
 
   cluster_name   = local.cluster_name
   argocd_project = module.eks.cluster_name
--- examples/eks/locals.tf
+++ examples/eks/locals.tf
@@ -1,7 +1,8 @@
 locals {
-  kubernetes_version       = "1.28"
+  kubernetes_version       = "1.29"
   cluster_name             = "YOUR_CLUSTER_NAME" # Must be unique for each DevOps Stack deployment in a single AWS account. Contains only alphanumeric and hyphens.
   base_domain              = "your.domain.here"  # Must match a Route 53 zone in the AWS account where you are deploying the DevOps Stack.
+  subdomain                = "apps"
   cluster_issuer           = module.cert-manager.cluster_issuers.staging
   letsencrypt_issuer_email = "YOUR_EMAIL_ADDRESS"
   enable_service_monitor   = false # Can be enabled after the first bootstrap.
